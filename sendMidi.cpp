7e6a18aaaea754383a8b80f78180f260a5083e91
Pieter Pas <pieter.pas@outlook.com>
2017-02-02 16:55:59 +0100

    Teensy 3.2 working

--- sendMidi.cpp
+++ sendMidi.cpp
@@ -6,7 +6,7 @@ byte pin;
 bool Blink;
 bool Debug;
 
-void setupMidi(byte p, int d, bool db = false){
+void setupMidi(byte p, int d, bool db){
 //#if defined (__AVR_ATmega328P__) || defined (__AVR_ATmega1280__) || defined (__AVR_ATmega2560__) // Only include these lines when compiling for Arduino Uno or Arduino Mega (that have an ATmega16U2 for serial communication)
 #if !defined(USBCON) // If you're compiling for an Arduino that has USB connection in the main MCU
   if(db)
