c3bdf925ca4636dd512f4bcc609c8688e99858ee
Pieter Pas <pieter.pas@outlook.com>
2017-02-02 17:37:56 +0100

    DFU scripts added

---
+++ DFU scripts/Linux/MIDI - Arduino-Uno-DFU.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
+cd "$parent_path"
+
+read -p "Are you sure you want to flash your ATmega16U2 with the HIDuino MIDI firmware? [Y/n]: " confirm
+if [ "$confirm" = y ] || [ "$confirm" = Y ]
+then 
+if [ ! -e arduino_midi.hex ]
+then
+echo "Downloading firmware: arduino_midi.hex ..."
+wget --quiet "https://raw.githubusercontent.com/ddiakopoulos/hiduino/master/compiled_firmwares/arduino_midi.hex"
+fi
+echo "Erasing ..."
+sudo dfu-programmer atmega16u2 erase
+echo "Flashing arduino_midi.hex ..."
+sudo dfu-programmer atmega16u2 flash arduino_midi.hex
+echo "Resetting ..."
+sudo dfu-programmer atmega16u2 reset
+echo "Done."
+fi
