a45757408b1918280319a11a8fb109bbf5b818fa
tttapa <pieter.pas@outlook.com>
2017-02-02 22:31:10 +0100

    RotaryEncoder is now independent of the PJRC Encoder library

--- src/RotaryEncoder.cpp
+++ src/RotaryEncoder.cpp
@@ -1,10 +1,44 @@
-
 #include "RotaryEncoder.h"
+#include "Encoder.h"
 
-// Yes, all the code is in the header file, to provide the user
-// configure options with #define (before they include it), and
-// to facilitate some crafty optimizations!
-
-Encoder_internal_state_t * RotaryEncoder::interruptArgs[];
-
-
+RotaryEncoder::RotaryEncoder(byte p, byte p2, byte n, byte c, int sp, byte pps, byte m) : enc(p,p2) // first pin, second pin, controller number, channel, speed multiplier, pulses  per step.
+{
+  controller = n;
+  channel = c;
+  speedMultiply = sp;
+  oldVal = 0;
+  pulsesPerStep = pps;
+  count = pps - 1;
+  mode = m;
+}
+void RotaryEncoder::refresh() {
+  int msgVal;
+  value = enc.read();
+  difference = (value - oldVal) / pulsesPerStep;
+  if (difference > 63) difference = 63;
+  if (difference < -63) difference = -63;
+  if (difference != 0) {
+    count = 0;
+    if (difference < 0) {
+      if (mode == 1) {
+        msgVal = 63 + difference * speedMultiply;
+      } else if (mode == 2) {
+        msgVal = bit(6) - difference * speedMultiply; // bit(6) gives 64, this is the sign bit
+      } else if (mode == 3) {
+        msgVal = 128 + difference * speedMultiply;
+      }
+      sendMidi(CC, channel, controller, msgVal);
+    }
+    else if ( difference > 0) {
+      if (mode == 1) {
+        msgVal = 63 + difference * speedMultiply;
+      } else if (mode == 2) {
+        msgVal = difference * speedMultiply;
+      } else if (mode == 3) {
+        msgVal = 0 + difference * speedMultiply;
+      }
+      sendMidi(CC, channel, controller, msgVal);
+    }
+    oldVal = value;
+  }
+}
