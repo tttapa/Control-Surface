9d2938f543b0631cfa0394d7ce1fee7c1efdc86e
tttapa <pieter.pas@outlook.com>
2017-02-03 11:52:49 +0100

    NO_BLINK fixed

--- src/sendMidi.cpp
+++ src/sendMidi.cpp
@@ -22,7 +22,8 @@ void setupMidi(byte p, int d, bool db){
     Serial.begin(9600); // a baud rate supported by the computer, for debugging
 #endif
 
-  Blink = true;  
+  if(p != NO_BLINK)
+    Blink = true;  
 #if !defined(USBCON) // If you're compiling for an Arduino that has no USB connection in the main MCU
   if(p == 0 || p == 1){ // If the user enters an invalid pin number (pins 0 and 1 are used for Serial communication) 
     Blink = false;
@@ -30,7 +31,8 @@ void setupMidi(byte p, int d, bool db){
 #endif
   
   pin = p;
-  if(Blink) pinMode(p,OUTPUT);  // The pin with the LED connected is set to output.
+  if(Blink) 
+    pinMode(p,OUTPUT);  // The pin with the LED connected is set to output.
   
   Delay = d;
   Debug = db;
@@ -39,7 +41,8 @@ void setupMidi(byte p, int d, bool db){
 void sendMidi(byte m, byte c, byte d1, byte d2) // Custom function to send MIDI messages: message, channel, data1, data2
 {
     c--; // Channels are zero-based
-  if(Blink) digitalWrite(pin,1);
+  if(Blink)
+    digitalWrite(pin,1);
   
   if(Debug){
     Serial.print((m>>4)&0xF, HEX); 
@@ -67,12 +70,11 @@ void sendMidi(byte m, byte c, byte d1, byte d2) // Custom function to send MIDI 
       unsigned int status : 4;    // first  nibble : status message (NOTE_ON, NOTE_OFF or CC (controlchange) 
       uint8_t data1;              // second byte   : first value (0-127), controller number or note number
       uint8_t data2;              // third  byte   : second value (0-127), controller value or velocity
-    } 
-    t_midiMsg;          // We call this structure 't_midiMsg'
+    } t_midiMsg;          // We call this structure 't_midiMsg'
 
     t_midiMsg msg;
     msg.status = (m>>4) & 0xF;
-    msg.channel = c & 0xF; // channels are 0-based
+    msg.channel = c & 0xF;
     msg.data1 = d1 & 0x7F;
     msg.data2 = d2 & 0x7F;
     Serial.write((uint8_t *)&msg, sizeof(msg));  // Send the MIDI message.
@@ -82,14 +84,16 @@ void sendMidi(byte m, byte c, byte d1, byte d2) // Custom function to send MIDI 
   if(Delay != 0){
     delay(Delay); // Prevent an overflow of MIDI messages. Increase to increase stability, decrease to achieve faster response.
   }
-  if(Blink) digitalWrite(pin,0);
+  if(Blink)
+    digitalWrite(pin,0);
 }
 
 
 void sendMidi(byte m, byte c, int d1) // Custom function to send MIDI messages: message, channel, data1
 {
   c--; // Channels are zero-based
- if(Blink) digitalWrite(pin,1);
+  if(Blink) 
+    digitalWrite(pin,1);
   
   if(Debug){
     Serial.print((m>>4)&0xF, HEX); 
@@ -113,13 +117,12 @@ void sendMidi(byte m, byte c, int d1) // Custom function to send MIDI messages: 
     typedef struct {
       unsigned int channel : 4;   // second nibble : midi channel (0-15) (channel and status are swapped, because Arduino is Little Endian)
       unsigned int status : 4;    // first  nibble : status message 
-      uint8_t data;              // second byte   : data  (0-127)
-    } 
-    t_midiMsg;          // We call this structure 't_midiMsg'
+      uint8_t data;               // second byte   : data  (0-127)
+    } t_midiMsg;          // We call this structure 't_midiMsg'
 
     t_midiMsg msg;
     msg.status = (m>>4) & 0xF;
-    msg.channel = c & 0xF; // channels are 0-based
+    msg.channel = c & 0xF;
     msg.data = d1 & 0x7F;
     Serial.write((uint8_t *)&msg, sizeof(msg));  // Send the MIDI message.
 #endif
@@ -128,6 +131,7 @@ void sendMidi(byte m, byte c, int d1) // Custom function to send MIDI messages: 
   if(Delay != 0){
     delay(Delay); // Prevent an overflow of MIDI messages. Increase to increase stability, decrease to achieve faster response.
   }
-  if(Blink) digitalWrite(pin,0);
+  if(Blink) 
+    digitalWrite(pin,0);
 }
 
