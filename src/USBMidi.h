0fdb26672b0457961f115242b134eb90aec364b6
Pieter <pieter.pas@outlook.com>
2017-07-29 11:44:19 +0200

    Add KEY_PRESSURE constant

--- src/USBMidi.h
+++ src/USBMidi.h
@@ -11,7 +11,7 @@
 
 const uint8_t NOTE_OFF = 0x80;
 const uint8_t NOTE_ON = 0x90;
-
+const uint8_t KEY_PRESSURE = 0xA0;
 const uint8_t CC = 0xB0;
 const uint8_t PROGRAM_CHANGE = 0xC0;
 const uint8_t CHANNEL_PRESSURE = 0xD0;
