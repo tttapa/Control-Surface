a7c10364af9bbd96cef8687672b105dec912c186
Pieter <pieter.pas@outlook.com>
2017-08-15 14:04:03 +0200

    Cleanup MIDI_controller.h

--- src/MIDI_controller.h
+++ src/MIDI_controller.h
@@ -1,14 +1,12 @@
 #ifndef MIDIctrl_h_
 #define MIDIctrl_h_
 
-#include "Arduino.h"
-
 #include "Analog.h"
 #include "AnalogHiRes.h"
+#include "ControlChange.h"
 #include "Digital.h"
 #include "DigitalLatch.h"
 #include "RotaryEncoder.h"
-#include "ControlChange.h"
 #include "USBMidi.h"
 
 #endif
