99dc4b4fcfe89cf279644afd00ab730f380cf637
Pieter <pieter.pas@outlook.com>
2017-08-18 16:11:49 +0200

    Explain different BankSelector modes

--- src/BankSelector.h
+++ src/BankSelector.h
@@ -27,12 +27,17 @@ public:
 
       BankSelector(bank, switch pin, BankSelector::TOGGLE);
 
+
   - One toggle switch (latching switch) and one LED
 
       When the switch is in the 'off' position, channel 1 is selected and the LED is off
       When the switch is in the 'on' position, channel 2 is selected and the LED is on
 
+          Note: this mode is pretty useless, you can just connect the LED to the switch directly,
+                without wasting a digital output pin on it.
+
       BankSelector(bank, switch pin, led pin, BankSelector::TOGGLE);
+
 
   - One momentary switch (push button)
 
@@ -45,6 +50,7 @@ public:
       BankSelector(bank, button pin);
       BankSelector(bank, button pin, BankSelector::MOMENTARY);
 
+
   - One momentary switch (push button) and one LED
 
       Pressing the button switches the channel and toggles the LED:
@@ -56,14 +62,80 @@ public:
       BankSelector(bank, button pin, led pin);
       BankSelector(bank, button pin, led pin, BankSelector::MOMENTARY);
 
+
   - Multiple momentary switches (push buttons)
 
-      Pressing one of the buttons selects the respective output
+      Pressing one of the buttons selects the respective output:
       When starting the program, channel 1 is selected,
       When the second button is pressed, channel 2 is selected,
       When the n-th button is pressed, channel n is selected.
 
       BankSelector(bank, { button 1 pin, button 2 pin, ... , button n pin } );
+
+
+  - Multiple momentary switches (push buttons) and multiple LEDs
+
+      Pressing one of the buttons selects the respective output and enables the respective LED:
+      When starting the program, channel 1 is selected and LED 1 is on,
+      When the second button is pressed, channel 2 is selected, LED 1 turns off and LED 2 turns on,
+      When the n-th button is pressed, channel n is selected, LED n turns on, and all other LEDs are off.
+
+      BankSelector(bank, { button 1 pin, button 2 pin, ... , button n pin }, { led 1 pin, led 2 pin, ... , led n pin } );
+
+  - Two momentary switches (push buttons)
+
+      Pressing the first button increments the channel number,
+      pressing the second button decrements the channel number: 
+      When starting the program, channel 1 is selected,
+      When the first button is pressed, channel 2 is selected, 
+      When the first button is pressed again, channel 3 is selected,
+      When the last channel is selected, and the first button is pressed again,
+      channel 1 is selected.
+      When the second button is pressed, the last channel (n) is selected,
+      When the second button is pressed again, channel (n-1) is selected,
+      and so on.
+
+      BankSelector(bank, { button increment pin, button decrement pin }, number of channels);
+
+
+  - Two momentary switches (push buttons) and multiple LEDs
+
+      Pressing the first button increments the channel number and turns on the respective LED,
+      pressing the second button decrements the channel number and turns on the respective LED: 
+      When starting the program, channel 1 is selected and LED 1 is on,
+      When the first button is pressed, channel 2 is selected, LED 1 turns off and LED 2 turns on,
+      When the first button is pressed again, channel 3 is selected, LED 2 turns off and LED 3 turns on.
+      When the last channel is selected, and the first button is pressed,
+      channel 1 is selected, the last LED turns off and LED 1 turns on.
+      When the second button is pressed, the last channel (n) is selected, LED 1 turns off and LED n turns on,
+      When the second button is pressed again, channel (n-1) is selected, LED n turns off and LED n-1 turns on,
+      and so on.
+
+      BankSelector(bank, { button increment pin, button decrement pin }, { led 1 pin, led 2 pin, ... , led n pin });
+
+
+  - One momentary switch (push button)
+
+      Pressing the button increments the channel number,
+      When starting the program, channel 1 is selected,
+      When the button is pressed, channel 2 is selected, 
+      When the button is pressed again, channel 3 is selected,
+      When the last channel is selected, and the button is pressed again,
+      channel 1 is selected.
+
+      BankSelector(bank, { button increment pin }, number of channels);
+
+
+  - One momentary switch (push button) and multiple LEDs
+
+      Pressing the button increments the channel number and turns on the respective LED,
+      When starting the program, channel 1 is selected and LED 1 is on,
+      When the button is pressed, channel 2 is selected, LED 1 turns off and LED 2 turns on,
+      When the button is pressed again, channel 3 is selected, LED 2 turns off and LED 3 turns on.
+      When the last channel is selected, and the button is pressed,
+      channel 1 is selected, the last LED turns off and LED 1 turns on.
+      
+      BankSelector(bank, { button increment pin }, { led 1 pin, led 2 pin, ... , led n pin });
 
 
     Note: a switch is 'off' or 'released' when it doesn't conduct. The digital value 
@@ -92,7 +164,7 @@ public:
     mode = MULTIPLE_BUTTONS;
   }
   template <size_t M, size_t N>
-  BankSelector(Bank &bank, const uint8_t (&switchPins)[M], const uint8_t (&ledPins)[N]) // Multiple buttons, multiple LEDs
+  BankSelector(Bank &bank, const uint8_t (&switchPins)[M], const uint8_t (&ledPins)[N]) // One or multiple buttons, multiple LEDs
       : bank(bank), switchPins(switchPins), ledPins(ledPins), nb_banks(N)
   {
     if (M == 1)
@@ -109,7 +181,7 @@ public:
       nb_banks = N < M ? N : M;
     }
   }
-  BankSelector(Bank &bank, std::initializer_list<uint8_t> switchPins, std::initializer_list<uint8_t> ledPins) // Multiple buttons, multiple LEDs
+  BankSelector(Bank &bank, std::initializer_list<uint8_t> switchPins, std::initializer_list<uint8_t> ledPins) // One or multiple buttons, multiple LEDs
       : bank(bank), switchPins(switchPins.begin()), ledPins(ledPins.begin()), nb_banks(ledPins.size())
   {
     if (switchPins.size() == 1)
@@ -136,7 +208,7 @@ public:
   {
     mode = INCREMENT;
   }
-  BankSelector(Bank &bank, std::initializer_list<uint8_t> switchPins, uint8_t nb_banks) // Two buttons (+1, -1), no LEDs
+  BankSelector(Bank &bank, std::initializer_list<uint8_t> switchPins, uint8_t nb_banks) // One or two buttons (+1, (-1)), no LEDs
       : bank(bank), switchPins(switchPins.begin()), nb_banks(nb_banks)
   {
     mode = switchPins.size() > 1 ? INCREMENT_DECREMENT : INCREMENT;
