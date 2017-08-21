0f91c1a77ffd1a0632284d233d4833046ae06610
Pieter <pieter.pas@outlook.com>
2017-08-21 22:39:48 +0200

    Delete bank functions Analog class

--- src/Analog.cpp
+++ src/Analog.cpp
@@ -10,9 +10,7 @@ Analog::Analog(uint8_t analogPin, uint8_t controllerNumber, uint8_t channel) // 
 
 Analog::~Analog() // Destructor
 {
-  free(avValues);            // free the sample buffer malloc'ed in Analog::average
-  if (bankEnabled)           // if bank mode was used
-    pinMode(bankPin, INPUT); // make make the bank switch pin a normal input again, without the internal pullup resistor.
+  free(avValues); // free the sample buffer malloc'ed in Analog::average
 }
 
 void Analog::average(size_t length) // use the average of multiple samples of analog readings
@@ -32,13 +30,10 @@ void Analog::refresh() // read the analog value, update the average, map it to a
   {                                // if averaging is enabled
     input = runningAverage(input); // update the running average with the new value
   }
-  value = input >> 3;  // map from the 10-bit analog input value [0, 1023] to the 7-bit MIDI value [0, 127]
-  if (value != oldVal) // if the value changed since last time
+  uint8_t value = input >> 3; // map from the 10-bit analog input value [0, 1023] to the 7-bit MIDI value [0, 127]
+  if (value != oldVal)        // if the value changed since last time
   {
-    if (bankEnabled && !digitalRead(bankPin))                       // if the bank mode is enabled, and the bank switch is in the 'alternative' position (i.e. if the switch is on (LOW))
-      USBMidiController.send(CC, altChannel + channelOffset, altController + addressOffset, value); // send a Control Change MIDI event with the 'alternative' channel and controller number
-    else                                                            // if the bank mode is disabled, or the bank switch is in the normal position
-      USBMidiController.send(CC, channel + channelOffset, controllerNumber + addressOffset, value); // send a Control Change MIDI event with the normal, original channel and controller number
+    USBMidiController.send(CC, channel + channelOffset, controllerNumber + addressOffset, value); // send a Control Change MIDI event
     oldVal = value;
   }
 }
@@ -46,25 +41,6 @@ void Analog::refresh() // read the analog value, update the average, map it to a
 void Analog::map(int (*fn)(int)) // change the function pointer for analogMap to a new function. It will be applied to the raw analog input value in Analog::refresh()
 {
   analogMap = fn;
-}
-
-void Analog::bank(uint8_t bankPin, uint8_t altController, uint8_t altChannel) // Enable the bank mode. When bank switch is turned on, send alternative MIDI channel and controller numbers
-{                                                                             // bankPin = digital pin with toggle switch connected
-  bankEnabled = true;
-
-  this->bankPin = bankPin;
-  pinMode(bankPin, INPUT_PULLUP);
-  this->altController = altController;
-  this->altChannel = altChannel;
-}
-
-void Analog::detachBank() // Disable the bank mode
-{
-  if (bankEnabled) // only defined if bank mode is enabled
-  {
-    bankEnabled = false;
-    pinMode(bankPin, INPUT); // make it a normal input again, without the internal pullup resistor.
-  }
 }
 
 unsigned int Analog::runningAverage(unsigned int value) // http://playground.arduino.cc/Main/RunningAverage
--- src/Analog.h
+++ src/Analog.h
@@ -8,23 +8,25 @@
 class Analog : public MIDI_Control_Element
 {
 public:
-  Analog(uint8_t analogPin, uint8_t controllerNumber, uint8_t channel);  // Constructor
-  ~Analog();                                                             // Deconstructor
-  void average(size_t length);                                           // Use the average of multiple samples of analog readings
-  void refresh();                                                        // Read the analog input value, update the average, map it to a MIDI value, check if it changed since last time, if so, send Control Change message over MIDI
-  void map(int (*fn)(int));                                              // Change the function pointer for analogMap to a new function. It will be applied to the raw analog input value in Analog::refresh()
-  void bank(uint8_t bankPin, uint8_t altController, uint8_t altChannel); // Enable the bank mode. When bank switch is turned on, send alternative MIDI channel and controller numbers
-  void detachBank();                                                     // Disable the bank mode
+  Analog(uint8_t analogPin, uint8_t controllerNumber, uint8_t channel); // Constructor
+  ~Analog();                                                            // Deconstructor
+  void average(size_t length);                                          // Use the average of multiple samples of analog readings
+  void refresh();                                                       // Read the analog input value, update the average, map it to a MIDI value, check if it changed since last time, if so, send Control Change message over MIDI
+  void map(int (*fn)(int));                                             // Change the function pointer for analogMap to a new function. It will be applied to the raw analog input value in Analog::refresh()
 
 private:
-  uint8_t analogPin, controllerNumber, channel, bankPin, altController, altChannel, value, oldVal = -1;
+  uint8_t analogPin, controllerNumber, channel, oldVal = -1;
   bool bankEnabled = false;
-  int (*analogMap)(int) = [](int x) { return x; }; // function pointer to identity function f(x) → x
+  int (*analogMap)(int) = identity; // function pointer to identity function f(x) → x
   size_t avLen = 0;
   unsigned int *avValues = nullptr;
   size_t avIndex = 0;
   unsigned long avSum = 0;
   size_t avCount = 0;
+
+  static int identity(int x) { // identity function f(x) → x
+    return x;
+  }
 
   unsigned int runningAverage(unsigned int value); // http://playground.arduino.cc/Main/RunningAverage
 };
