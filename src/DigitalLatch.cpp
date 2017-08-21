69523e7bd8694e52cceb768cbf2b00a6ccb74c8f
Pieter <pieter.pas@outlook.com>
2017-08-21 23:51:03 +0200

    Delete banks in DigitalLatch class

--- src/DigitalLatch.cpp
+++ src/DigitalLatch.cpp
@@ -12,9 +12,7 @@ DigitalLatch::DigitalLatch(uint8_t pin, uint8_t note, uint8_t channel, uint8_t v
 
 DigitalLatch::~DigitalLatch() // Deconstructor
 {
-  pinMode(pin, INPUT);       // make it a normal input again, without the internal pullup resistor.
-  if (bankEnabled)           // if bank mode was used
-    pinMode(bankPin, INPUT); // make make the bank switch pin a normal input again, without the internal pullup resistor.
+  pinMode(pin, INPUT); // make it a normal input again, without the internal pullup resistor.
 }
 
 void DigitalLatch::refresh() // Check if the button state changed, if so, send a MIDI Note On, after a non-blocking delay of "latchTime", send a Note Off
@@ -24,53 +22,21 @@ void DigitalLatch::refresh() // Check if the button state changed, if so, send a
   {
     if (noteOffSent) // If the note is turned off
     {
-      sendNote(NOTE_ON);     // Turn on the note
-      noteOnTime = millis(); // store the time of the note on message
-      noteOffSent = false;   // The note is turned on
+      USBMidiController.send(NOTE_ON, channel + channelOffset, note + addressOffset, velocity); // Turn on the note
+      noteOnTime = millis();                                                                    // store the time of the note on message
+      noteOffSent = false;                                                                      // The note is turned on
     }
     else // If the button is switched again, before latch time is reached
     {
-      sendNote(NOTE_OFF);    // Turn off the note
-      sendNote(NOTE_ON);     // Immediately turn the note on again
-      noteOnTime = millis(); // store the time of the note on message
+      USBMidiController.send(NOTE_OFF, channel + channelOffset, note + addressOffset, velocity); // Turn off the note
+      USBMidiController.send(NOTE_ON, channel + channelOffset, note + addressOffset, velocity);  // Immediately turn the note on again
+      noteOnTime = millis();                                                                     // store the time of the note on message
     }
     oldState = state;
   }
   if (millis() - noteOnTime > latchTime && !noteOffSent) // if the time elapsed since the Note On event is greater than the latch time, and if the note is still on
   {
-    sendNote(NOTE_OFF); // Turn off the note
-    noteOffSent = true; // The note is turned off
+    USBMidiController.send(NOTE_OFF, channel + channelOffset, note + addressOffset, velocity); // Turn off the note
+    noteOffSent = true;                                                                        // The note is turned off
   }
-}
-
-void DigitalLatch::bank(uint8_t bankPin, uint8_t altNote, uint8_t altChannel) // Enable the bank mode. When bank switch is turned on, send alternative MIDI channel and controller numbers
-{                                                                             // bankPin = digital pin with toggle switch connected
-  bankEnabled = true;
-
-  this->bankPin = bankPin;
-  pinMode(bankPin, INPUT_PULLUP);
-  this->altNote = altNote;
-  this->altChannel = altChannel;
-}
-
-void DigitalLatch::detachBank() // Disable the bank mode
-{
-  if (bankEnabled) // only defined if bank mode is enabled
-  {
-    bankEnabled = false;
-    pinMode(bankPin, INPUT); // make it a normal input again, without the internal pullup resistor
-  }
-}
-
-void DigitalLatch::sendNote(uint8_t noteOnOrOff) // turn on or off a note, select the channel and note number based on the bank mode and bank switch state
-{
-  if (bankEnabled && !digitalRead(bankPin))                             // if the bank mode is enabled, and the bank switch is in the 'alternative' position (i.e. if the switch is on (LOW))
-    USBMidiController.send(noteOnOrOff, altChannel, altNote, velocity); // send a Note On or Off MIDI event with the 'alternative' channel and note number
-  else                                                                  // if the bank mode is disabled, or the bank switch is in the normal position
-    USBMidiController.send(noteOnOrOff, channel, note, velocity);       // send a Note On or Off MIDI event with the normal, original channel and note number
-}
-
-void DigitalLatch::setChannel(uint8_t channel) // setter for the MIDI channel
-{
-  this->channel = channel;
 }
--- src/DigitalLatch.h
+++ src/DigitalLatch.h
@@ -11,20 +11,13 @@ public:
   DigitalLatch(uint8_t pin, uint8_t note, uint8_t channel, uint8_t velocity, unsigned long latchTime); // Constructor
   ~DigitalLatch();                                                                                     // Deconstructor
   void refresh();                                                                                      // Check if the button state changed, if so, send a MIDI Note On, after a non-blocking delay of "latchTime", send a Note Off
-  void bank(uint8_t bankPin, uint8_t altNote, uint8_t altChannel);                                     // Enable the bank mode. When bank switch is turned on, send alternative MIDI channel and controller numbers
-  void detachBank();                                                                                   // Disable the bank mode
-
-  void setChannel(uint8_t channel); // setter for the MIDI channel
 
 private:
-  uint8_t pin, note, channel, velocity, bankPin, altNote, altChannel;
+  uint8_t pin, note, channel, velocity;
   bool oldState = HIGH;
   bool noteOffSent = true;
-  bool bankEnabled = false;
   unsigned long latchTime;
   unsigned long noteOnTime;
-
-  void sendNote(uint8_t noteOnOrOff); // turn on or off a note, select the channel and note number based on the bank mode and bank switch state
 };
 
 #endif
