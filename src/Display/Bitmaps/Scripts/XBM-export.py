c387267d5900b0015962b99d342e9f5bce45b8c5
Pieter <pieter.pas@outlook.com>
2018-04-01 22:14:09 +0200

    Makefile for documentation, cleanup XBM-export.py, subfolders for XBM

---
+++ src/Display/Bitmaps/Scripts/XBM-export.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+"""
+Converts XBM files to header files that 
+can be included in Arduino source code. 
+Bitmaps are saved to program memory (PROGMEM).
+
+The filename (without extension) is used 
+as the identifier for the bitmap.
+
+A header file is created that includes all 
+these bitmaps (as a XBitmap struct), including
+Doxygen documentation.
+All XBM images are converted to PNG images
+and saved to the Doxygen HTML folder, to be
+displayed in the documentation.
+"""
+
+# TODO: make this code readable.
+
+import os
+import re
+import png
+
+bitmaps = []
+
+dirnm = os.path.dirname(os.path.realpath(__file__))
+
+inputdir = os.path.realpath(os.path.join(dirnm,  '../XBM'))
+outputdir = os.path.realpath(os.path.join(dirnm, '..'))
+# pngoutputdir = os.path.realpath(os.path.join(dirnm, '../PNG'))
+pngoutputdir = os.path.realpath(os.path.join(dirnm, '../../../../doc/Doxygen/html'))
+
+def XBM2PNG(bytedata):
+    index = 0
+    mask = 1
+    imglist = []
+    for row in range(int(height)):
+        if mask != 1:
+            index += 1
+            mask = 1
+        rowlist = []
+        for col in range(int(width)):
+            color = 255*int(not bool(bytedata[index] & mask))
+            rowlist.append(color)
+            mask <<= 1
+            if mask & 0xFF == 0:
+                mask = 1
+                index += 1
+        imglist.append(rowlist)
+    return png.from_array(imglist, 'L')
+
+for filename in sorted(os.listdir(inputdir)):
+    if filename.endswith(".xbm"):
+        print(filename)
+        with open(os.path.join(inputdir, filename), 'r') as file:
+            contents = file.read()
+        identifier = os.path.splitext(filename)[0]
+        contents = re.sub(r'(#define )[a-zA-Z_][a-zA-Z0-9_]*((?:(_width)|(_height)) +\d+)',r'\1{}\2'.format(identifier),contents)
+        contents = re.sub(r'static unsigned char [a-zA-Z_][a-zA-Z0-9_]*(_bits\[\] *= *{ *(?:0x[0-9a-fA-F]{2}, *)*}?;?) *',r'static const PROGMEM uint8_t {}\1'.format(identifier),contents)
+        m = re.search(r'#define [a-zA-Z\d_]+_width\s+(?P<width>\d+)[\r\n\s]+#define [a-zA-Z\d_]+_height\s+(?P<height>\d+)', contents)
+        if m:
+            width, height = m.group('width', 'height')
+            print('\t({}px × {}px)'.format(width, height))
+            m = re.search(r'{[\r\n\s]*((?:0x[a-fA-F0-9]{2},?[\r\n\s]*)+)}', contents)
+            if m:
+                data = m.group(1)
+                data = re.sub(r'[\r\n\s]|(?:0x)','', data)
+                data = re.sub(r',',' ', data)
+                bytedata = bytearray.fromhex(data)
+                PNG = XBM2PNG(bytedata)
+                PNG.save(os.path.join(pngoutputdir, identifier+'.png'))
+                bitmaps.append((identifier,width,height))
+
+                with open(os.path.join(outputdir, identifier+'.axbm'), 'w') as axbm:
+                    axbm.write(contents)
+            else:
+                print('Warning: invalid XBM data')
+        else:
+            print('Warning: invalid XBM width or height data')
+        print()
+
+XBitmaps = ''
+for bm in bitmaps:
+    id = bm[0]
+    width = bm[1]
+    height = bm[2]
+
+    XBitmaps += \
+"""#include "{id}.axbm"
+/**
+ * const XBitmap&emsp;<b>{id}</b>&emsp;({width}px × {height}px)
+ * <img src="{id}.png" alt="{id}" class="xbm" style="width:{pngwidth}em;">
+ */
+MakeXBitmap({id});
+
+""".format(id=id, width=width, height=height, pngwidth=(float(width)*0.75))
+
+with open(os.path.join(outputdir, 'XBitmaps.template'), 'r') as template:
+    XBitmaps = re.sub(r':contents', XBitmaps, template.read())
+
+with open(os.path.join(outputdir, 'XBitmaps.h'), 'w') as XBitmapsFile:
+    XBitmapsFile.write(XBitmaps)
--- src/Display/Bitmaps/XBitmaps.h
+++ src/Display/Bitmaps/XBitmaps.h
@@ -22,101 +22,101 @@ namespace XBM
 
 // =========================================================================== //
 
-#include "mute_10B.arduinoxbm"
+#include "mute_10B.axbm"
 /**
  * const XBitmap&emsp;<b>mute_10B</b>&emsp;(10px × 10px)
- * <img src="mute_10B.png" alt="" style="image-rendering:pixelated;image-rendering:-moz-crisp-edges;width:7.5em;border:1px solid #ccc">
+ * <img src="mute_10B.png" alt="mute_10B" class="xbm" style="width:7.5em;">
  */
 MakeXBitmap(mute_10B);
 
-#include "mute_14B.arduinoxbm"
+#include "mute_14B.axbm"
 /**
  * const XBitmap&emsp;<b>mute_14B</b>&emsp;(14px × 14px)
- * <img src="mute_14B.png" alt="" style="image-rendering:pixelated;image-rendering:-moz-crisp-edges;width:10.5em;border:1px solid #ccc">
+ * <img src="mute_14B.png" alt="mute_14B" class="xbm" style="width:10.5em;">
  */
 MakeXBitmap(mute_14B);
 
-#include "mute_7.arduinoxbm"
+#include "mute_7.axbm"
 /**
  * const XBitmap&emsp;<b>mute_7</b>&emsp;(7px × 7px)
- * <img src="mute_7.png" alt="" style="image-rendering:pixelated;image-rendering:-moz-crisp-edges;width:5.25em;border:1px solid #ccc">
+ * <img src="mute_7.png" alt="mute_7" class="xbm" style="width:5.25em;">
  */
 MakeXBitmap(mute_7);
 
-#include "play_10x9.arduinoxbm"
+#include "play_10x9.axbm"
 /**
  * const XBitmap&emsp;<b>play_10x9</b>&emsp;(10px × 9px)
- * <img src="play_10x9.png" alt="" style="image-rendering:pixelated;image-rendering:-moz-crisp-edges;width:7.5em;border:1px solid #ccc">
+ * <img src="play_10x9.png" alt="play_10x9" class="xbm" style="width:7.5em;">
  */
 MakeXBitmap(play_10x9);
 
-#include "play_7.arduinoxbm"
+#include "play_7.axbm"
 /**
  * const XBitmap&emsp;<b>play_7</b>&emsp;(7px × 7px)
- * <img src="play_7.png" alt="" style="image-rendering:pixelated;image-rendering:-moz-crisp-edges;width:5.25em;border:1px solid #ccc">
+ * <img src="play_7.png" alt="play_7" class="xbm" style="width:5.25em;">
  */
 MakeXBitmap(play_7);
 
-#include "play_8x7.arduinoxbm"
+#include "play_8x7.axbm"
 /**
  * const XBitmap&emsp;<b>play_8x7</b>&emsp;(8px × 7px)
- * <img src="play_8x7.png" alt="" style="image-rendering:pixelated;image-rendering:-moz-crisp-edges;width:6.0em;border:1px solid #ccc">
+ * <img src="play_8x7.png" alt="play_8x7" class="xbm" style="width:6.0em;">
  */
 MakeXBitmap(play_8x7);
 
-#include "rec_rdy_10B.arduinoxbm"
+#include "rec_rdy_10B.axbm"
 /**
  * const XBitmap&emsp;<b>rec_rdy_10B</b>&emsp;(10px × 10px)
- * <img src="rec_rdy_10B.png" alt="" style="image-rendering:pixelated;image-rendering:-moz-crisp-edges;width:7.5em;border:1px solid #ccc">
+ * <img src="rec_rdy_10B.png" alt="rec_rdy_10B" class="xbm" style="width:7.5em;">
  */
 MakeXBitmap(rec_rdy_10B);
 
-#include "rec_rdy_14B.arduinoxbm"
+#include "rec_rdy_14B.axbm"
 /**
  * const XBitmap&emsp;<b>rec_rdy_14B</b>&emsp;(14px × 14px)
- * <img src="rec_rdy_14B.png" alt="" style="image-rendering:pixelated;image-rendering:-moz-crisp-edges;width:10.5em;border:1px solid #ccc">
+ * <img src="rec_rdy_14B.png" alt="rec_rdy_14B" class="xbm" style="width:10.5em;">
  */
 MakeXBitmap(rec_rdy_14B);
 
-#include "rec_rdy_7.arduinoxbm"
+#include "rec_rdy_7.axbm"
 /**
  * const XBitmap&emsp;<b>rec_rdy_7</b>&emsp;(7px × 7px)
- * <img src="rec_rdy_7.png" alt="" style="image-rendering:pixelated;image-rendering:-moz-crisp-edges;width:5.25em;border:1px solid #ccc">
+ * <img src="rec_rdy_7.png" alt="rec_rdy_7" class="xbm" style="width:5.25em;">
  */
 MakeXBitmap(rec_rdy_7);
 
-#include "record_7.arduinoxbm"
+#include "record_7.axbm"
 /**
  * const XBitmap&emsp;<b>record_7</b>&emsp;(7px × 7px)
- * <img src="record_7.png" alt="" style="image-rendering:pixelated;image-rendering:-moz-crisp-edges;width:5.25em;border:1px solid #ccc">
+ * <img src="record_7.png" alt="record_7" class="xbm" style="width:5.25em;">
  */
 MakeXBitmap(record_7);
 
-#include "record_9.arduinoxbm"
+#include "record_9.axbm"
 /**
  * const XBitmap&emsp;<b>record_9</b>&emsp;(9px × 9px)
- * <img src="record_9.png" alt="" style="image-rendering:pixelated;image-rendering:-moz-crisp-edges;width:6.75em;border:1px solid #ccc">
+ * <img src="record_9.png" alt="record_9" class="xbm" style="width:6.75em;">
  */
 MakeXBitmap(record_9);
 
-#include "solo_10B.arduinoxbm"
+#include "solo_10B.axbm"
 /**
  * const XBitmap&emsp;<b>solo_10B</b>&emsp;(10px × 10px)
- * <img src="solo_10B.png" alt="" style="image-rendering:pixelated;image-rendering:-moz-crisp-edges;width:7.5em;border:1px solid #ccc">
+ * <img src="solo_10B.png" alt="solo_10B" class="xbm" style="width:7.5em;">
  */
 MakeXBitmap(solo_10B);
 
-#include "solo_14B.arduinoxbm"
+#include "solo_14B.axbm"
 /**
  * const XBitmap&emsp;<b>solo_14B</b>&emsp;(14px × 14px)
- * <img src="solo_14B.png" alt="" style="image-rendering:pixelated;image-rendering:-moz-crisp-edges;width:10.5em;border:1px solid #ccc">
+ * <img src="solo_14B.png" alt="solo_14B" class="xbm" style="width:10.5em;">
  */
 MakeXBitmap(solo_14B);
 
-#include "solo_7.arduinoxbm"
+#include "solo_7.axbm"
 /**
  * const XBitmap&emsp;<b>solo_7</b>&emsp;(7px × 7px)
- * <img src="solo_7.png" alt="" style="image-rendering:pixelated;image-rendering:-moz-crisp-edges;width:5.25em;border:1px solid #ccc">
+ * <img src="solo_7.png" alt="solo_7" class="xbm" style="width:5.25em;">
  */
 MakeXBitmap(solo_7);
 
