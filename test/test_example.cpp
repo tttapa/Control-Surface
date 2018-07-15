56dc93e7b71e3348c1fa54dfd400325049b2f037
Pieter <pieter.pas@outlook.com>
2018-07-15 14:11:07 +0200

    Gmock working

--- test/test_example.cpp
+++ test/test_example.cpp
@@ -1,6 +1,6 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
 #include <Arduino.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
 using namespace ::testing;
 
@@ -16,4 +16,28 @@ TEST(millis, millis) {
         .WillRepeatedly(Return(2));
     EXPECT_EQ(millis(), 1);
     EXPECT_EQ(millis(), 2);
+}
+
+TEST(Serial, write_byte) {
+    EXPECT_CALL(Serial, write(0x12));
+    Serial.write(0x12);
+}
+TEST(Serial, write_bytes) {
+    EXPECT_CALL(Serial, write(_, _))
+        .With(Args<0, 1>(ElementsAre(0x00, 0x01, 0x02)));
+    const uint8_t buff[] = {0x00, 0x01, 0x02};
+    Serial.write(buff, 3);
+}
+TEST(Serial, print_string) {
+    EXPECT_CALL(Serial, write(_, _))
+        .With(Args<0, 1>(ElementsAre('T', 'e', 's', 't')));
+    const char *str = "Test";
+    Serial.print(str);
+}
+TEST(Serial, println_string) {
+    EXPECT_CALL(Serial, write(_, _))
+        .With(Args<0, 1>(ElementsAre('T', 'e', 's', 't')));
+    EXPECT_CALL(Serial, write(_, _)).With(Args<0, 1>(ElementsAre('\r', '\n')));
+    const char *str = "Test";
+    Serial.println(str);
 }
