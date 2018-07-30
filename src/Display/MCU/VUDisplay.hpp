121b790414d4990f57db9887fd47edcc7e3fba32
Pieter <pieter.pas@outlook.com>
2018-07-30 21:54:47 +0200

    Decay smoothly VU

--- src/Display/MCU/VUDisplay.hpp
+++ src/Display/MCU/VUDisplay.hpp
@@ -12,12 +12,14 @@ class VUDisplay : public DisplayElement {
               uint16_t color)
         : DisplayElement(display), vu(vu), x(loc.x), y(loc.y - blockheight + 1),
           width(width), blockheight(blockheight), spacing(spacing),
-          color(color) {}
+          color(color), decayTime(decayTimePerBlock / (blockheight + spacing)) {
+    }
     void draw() {
         uint8_t value = vu.getValue();
 
-        if (value >= peak) {
-            peak = value;
+        int16_t newPeak = value * (blockheight + spacing);
+        if (newPeak >= peak) {
+            peak = newPeak;
             previousDecay = millis();
         } else if (millis() - previousDecay > decayTime) {
             if (peak > 0) {
@@ -26,9 +28,7 @@ class VUDisplay : public DisplayElement {
             }
         }
         if (peak > 0) {
-            display.drawFastHLine(
-                x, y - spacing + blockheight - peak * (blockheight + spacing),
-                width, color);
+            display.drawFastHLine(x, y - spacing + blockheight - peak, width, color);
             for (uint8_t i = 0; i < value; i++)
                 display.fillRect(x, y - i * (blockheight + spacing), width,
                                  blockheight, color);
@@ -45,10 +45,12 @@ class VUDisplay : public DisplayElement {
     uint8_t spacing;
     uint16_t color;
 
-    unsigned long peak = 0;
+    int16_t peak = 0;
     unsigned long previousDecay = 0;
 
-    constexpr static unsigned long decayTime = 350;
+    unsigned long decayTime;
+
+    constexpr static unsigned long decayTimePerBlock = 350; // TODO: Settings.h
 };
 
 } // namespace MCU
