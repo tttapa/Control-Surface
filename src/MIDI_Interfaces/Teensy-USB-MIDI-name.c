bb3ca73f6ffeef3fea11d2095fcdff708a9ba6e7
Pieter <pieter.pas@outlook.com>
2018-08-03 22:03:31 +0200

    Add Teensy MIDI USB name

---
+++ src/MIDI_Interfaces/Teensy-USB-MIDI-name.c
@@ -0,0 +1,14 @@
+#if defined (ARDUINO) && defined (TEENSYDUINO)
+
+#include "usb_names.h"
+
+#define MIDI_NAME   {'C','o','n','t','r','o','l',' ','S','u','r','f','a','c','e'}
+#define MIDI_NAME_LEN  15
+
+struct usb_string_descriptor_struct usb_string_product_name = {
+        2 + MIDI_NAME_LEN * 2,
+        3,
+        MIDI_NAME
+};
+
+#endif
