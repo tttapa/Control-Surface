a422a5318be37f8008fe9b51bae1760504d2bacc
Pieter Pas <pieter.pas@outlook.com>
2019-06-26 21:59:48 +0200

    Fix arduino builder for ESP32

--- scripts/build-arduino.sh
+++ scripts/build-arduino.sh
@@ -22,9 +22,20 @@ build_with_lock(){
     echo -e "\e[1;94mBuilding Example: $name\e[0m"
     if [ $result -ne 0 ]; then failed=$[$failed + 1]; fi
     (
+        ### Setup for arduino-builder
+        arduino=$HOME/opt/arduino-1.8.9
+        version=10809
+        libraries=$HOME/Arduino/libraries
+
+        if grep -q "ESP32 only" "$file"; then
+            board=esp32:esp32:esp32thing:FlashFreq=80,PartitionScheme=default,UploadSpeed=921600,DebugLevel=none
+        else
+            board=teensy:avr:teensy31:speed=96,usb=serialmidiaudio,opt=o2std,keys=en-us
+        fi
+
         arduino-builder \
-            -hardware $arduino/hardware/ \
-            -tools $arduino/tools-builder \
+            -hardware $arduino/hardware -hardware $HOME/.arduino15/packages \
+            -tools $arduino/tools-builder -tools $HOME/.arduino15/packages \
             -libraries $libraries \
             -fqbn $board \
             -core-api-version $version \
@@ -38,7 +49,6 @@ build_with_lock(){
             echo -e "\e[1;31mBuilding $name failed!\e[0m\n"
             realpath $1
         fi
-        
         printf '%.3d' $res >&3
     )&
 }
@@ -66,12 +76,7 @@ elif [ "$#" -eq 1 ]; then
 fi
 echo Building $N examples concurrently
 
-### Setup for arduino-builder
-board=teensy:avr:teensy31:speed=96,usb=serialmidiaudio,opt=o2std,keys=en-us
-arduino=$HOME/opt/arduino-1.8.9
-version=10809
-libraries=$HOME/Arduino/libraries
-
+### Find files to compile
 test_files=($(find ../test -name '*.ino'))
 example_files=($(find ../examples -name '*.ino'))
 files=("${test_files[@]}" "${example_files[@]}")
