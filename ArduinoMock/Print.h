7fdb5eb1aa88bf10eef7aa1427ff716efbd9331b
Pieter Pas <pieter.pas@outlook.com>
2019-07-06 00:51:57 +0200

    Mock/fallback Printable

--- ArduinoMock/Print.h
+++ ArduinoMock/Print.h
@@ -25,7 +25,7 @@
 
 #include <string.h>
 
-#include "Printable.h"
+class Printable;
 
 #define DEC 10
 #define HEX 16
@@ -35,28 +35,30 @@
 #endif
 #define BIN 2
 
-class Print
-{
+class Print {
   private:
     int write_error;
     size_t printNumber(unsigned long, uint8_t);
     size_t printFloat(double, uint8_t);
+
   protected:
     void setWriteError(int err = 1) { write_error = err; }
+
   public:
     Print() : write_error(0) {}
-  
+
     int getWriteError() { return write_error; }
     void clearWriteError() { setWriteError(0); }
-  
+
     virtual size_t write(uint8_t) = 0;
     size_t write(const char *str) {
-      if (str == NULL) return 0;
-      return write((const uint8_t *)str, strlen(str));
+        if (str == NULL)
+            return 0;
+        return write((const uint8_t *)str, strlen(str));
     }
     virtual size_t write(const uint8_t *buffer, size_t size);
     size_t write(const char *buffer, size_t size) {
-      return write((const uint8_t *)buffer, size);
+        return write((const uint8_t *)buffer, size);
     }
 
     // default to zero, meaning "a single write may block"
@@ -71,7 +73,7 @@ class Print
     size_t print(long, int = DEC);
     size_t print(unsigned long, int = DEC);
     size_t print(double, int = 2);
-    size_t print(const Printable&);
+    size_t print(const Printable &);
 
     size_t println(const char[]);
     size_t println(char);
@@ -81,10 +83,26 @@ class Print
     size_t println(long, int = DEC);
     size_t println(unsigned long, int = DEC);
     size_t println(double, int = 2);
-    size_t println(const Printable&);
+    size_t println(const Printable &);
     size_t println(void);
 
-    virtual void flush() { /* Empty implementation for backward compatibility */ }
+    virtual void flush() { /* Empty implementation for backward compatibility */
+    }
 };
+
+#include <ostream>
+
+class OstreamPrint : public Print {
+    std::ostream &os;
+
+  public:
+    OstreamPrint(std::ostream &os) : os(os) {}
+    size_t write(uint8_t c) override {
+        os.put(c);
+        return 1;
+    }
+};
+
+#include "Printable.h"
 
 #endif
