8423c6b4bfe0f62673b105ae5dab265773f2dd63
Pieter Pas <pieter.pas@outlook.com>
2019-08-04 14:21:35 +0200

    Add coverage target and task

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -14,6 +14,7 @@ rm -f "$dest"/*.info
 rm -rf "$dest"/html/*
 
 gcov_bin="gcov-9"
+branches=1
 
 cd "$proj_dir"
 pwd
@@ -27,20 +28,23 @@ make -C "$build_dir" -j$((`nproc` * 2))
 lcov \
     --capture --initial --directory . \
     --output-file "$dest"/coverage_base.info \
-    --gcov-tool $gcov_bin
+    --gcov-tool $gcov_bin \
+    --rc lcov_branch_coverage=$branches
 
 make -C "$build_dir" check
 
 lcov \
     --capture --directory . \
     --output-file "$dest"/coverage_test.info \
-    --gcov-tool $gcov_bin
+    --gcov-tool $gcov_bin \
+    --rc lcov_branch_coverage=$branches
 
 lcov \
     --add-tracefile "$dest"/coverage_base.info \
     --add-tracefile "$dest"/coverage_test.info \
     --output-file "$dest"/coverage_total.info \
-    --gcov-tool $gcov_bin
+    --gcov-tool $gcov_bin \
+    --rc lcov_branch_coverage=$branches
 
 lcov \
     --remove "$dest"/coverage_total.info \
@@ -49,10 +53,12 @@ lcov \
     '*/googletest/*' \
     '*/test/*' \
     --output-file "$dest"/coverage_filtered.info \
-    --gcov-tool $gcov_bin
+    --gcov-tool $gcov_bin \
+    --rc lcov_branch_coverage=$branches
 
 genhtml \
     --prefix `realpath "$proj_dir"` \
     "$dest"/coverage_filtered.info \
     --output-directory="$dest"/html \
-    --legend --title `cd "$proj_dir" && git rev-parse HEAD`
+    --legend --title `cd "$proj_dir" && git rev-parse HEAD` \
+    --rc lcov_branch_coverage=$branches
