b1b2f0c978831dfd885a63e34af1dbf10a4ba693
Pieter Pas <pieter.pas@outlook.com>
2019-08-13 11:33:46 +0200

    Add CCPotentiometer example

---
+++ examples/1. MIDI Output/1. Potentiometers & Faders/Control-Change-Potentiometer/Control-Change-Potentiometer.ino
@@ -0,0 +1,52 @@
+/** 
+ * This example demonstrates the use of MIDI Control Change potentiometers that
+ * can be used for changing effect parameters, volumes, pan and balance
+ * controls, etc. It can control almost any knob in your DAW software.
+ *
+ * @boards  AVR, AVR USB, Teensy 3.x, ESP32
+ * 
+ * Connections
+ * -----------
+ * 
+ * - A0: wiper of a 10 potentiometer
+ * 
+ * Connect the left terminal of the potentiometer to ground, and the right one
+ * to V<sub>CC</sub>.
+ * 
+ * Behavior
+ * --------
+ * 
+ * - When you turn the potentiometer, you should receive MIDI Control Change
+ *   events, with a value between 0 and 127.
+ * - The analog input is filtered, so there shouldn't be any noise on the 
+ *   position. If there is, check your wiring, and make sure that the resistance
+ *   of the potentiometer isn't too high (10 kΩ is ideal).
+ * 
+ * Mapping
+ * -------
+ * 
+ * Select the Arduino as a custom MIDI controller in your DAW, and use the 
+ * MIDI learn option to assign the potentiometer to a function.
+ * 
+ * Written by PieterP, 2019-08-13  
+ * https://github.com/tttapa/Control-Surface
+ */
+
+#include <Control_Surface.h> // Include the Control Surface library
+
+// Instantiate a MIDI over USB interface.
+USBMIDI_Interface midi;
+
+// Instantiate a NoteButton object
+CCPotentiometer potentiometer = {
+  A0,                                   // Analog pin connected to potentiometer
+  {MIDI_CC::Channel_Volume, CHANNEL_1}, // Channel volume of channel 1
+};
+
+void setup() {
+  Control_Surface.begin(); // Initialize Control Surface
+}
+
+void loop() {
+  Control_Surface.loop(); // Update the Control Surface
+}
