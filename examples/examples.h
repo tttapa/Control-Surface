f927fd680052aa5997c441c0098e211f0afa4fb9
Pieter Pas <pieter.pas@outlook.com>
2019-08-22 23:46:35 +0200

    Add MultiMIDI_Interface

--- examples/examples.h
+++ examples/examples.h
@@ -574,6 +574,43 @@
  */
 
 /**
+ * @example   "Dual-MIDI-Interface.ino"
+ * 
+ * Dual-MIDI-Interface
+ * ===================
+ * 
+ * This example demonstrates how to group together two or more MIDI interfaces,
+ * so you can send the MIDI output over both MIDI USB and 5-pin DIN MIDI, for 
+ * example. MIDI input is received from all interfaces as well.
+ *
+ * @boards  AVR USB, Teensy 3.x
+ * 
+ * Connections
+ * -----------
+ * 
+ * - A0: wiper of a potentiometer
+ * 
+ * Connect the left terminal of the potentiometer to ground, and the right one
+ * to V<sub>CC</sub>.
+ * 
+ * Behavior
+ * --------
+ * 
+ * - When you turn the potentiometer, you should receive MIDI Control Change
+ *   events on both the Serial MIDI interface and the USB MIDI interface.
+ * - When a MIDI Note event for middle C is received on either MIDI interface,
+ *   the state of the built-in LED will change accordingly.
+ * 
+ * Mapping
+ * -------
+ * 
+ * - None
+ * 
+ * Written by PieterP, 2019-08-22  
+ * https://github.com/tttapa/Control-Surface
+ */
+
+/**
  * @example   "1.Note-LED.ino"
  * 
  * 1.Note-LED
---
+++ src/MIDI_Interfaces/MultiMIDI_Interface.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <MIDI_Interfaces/MIDI_Interface.hpp>
+
+/**
+ * @brief   A class for grouping multiple MIDI interfaces in parallel.
+ *          Output is sent to all @c N interfaces, and incoming messages are 
+ *          received from all @c N interfaces.
+ * 
+ * @tparam  N
+ *          The number of interfaces.
+ */
+template <uint8_t N>
+class MultiMIDI_Interface : public MIDI_Interface {
+  public:
+    /**
+     * @brief   Constructor.
+     * 
+     * @param   interfaces 
+     *          An array of pointers to the MIDI interfaces to group together.
+     *          Only the pointers are saved, the MIDI interfaces themselves must
+     *          outlive this MultiMIDI_Interface.
+     */
+    MultiMIDI_Interface(const Array<Parsing_MIDI_Interface *, N> &interfaces)
+        : interfaces(interfaces) {}
+
+    void update() override {
+        for (auto interface : interfaces)
+            interface->update();
+    }
+
+    void setCallbacks(MIDI_Callbacks *cb) override {
+        for (auto interface : interfaces)
+            interface->setCallbacks(cb);
+    }
+
+  protected:
+    void sendImpl(uint8_t m, uint8_t c, uint8_t d1, uint8_t d2,
+                  uint8_t cn) override {
+        for (auto interface : interfaces)
+            interface->sendImpl(m, c, d1, d2, cn);
+    }
+
+    void sendImpl(uint8_t m, uint8_t c, uint8_t d1, uint8_t cn) override {
+        for (auto interface : interfaces)
+            interface->sendImpl(m, c, d1, cn);
+    }
+
+    void sendImpl(const uint8_t *data, size_t length, uint8_t cn) override {
+        for (auto interface : interfaces)
+            interface->sendImpl(data, length, cn);
+    }
+
+  private:
+    Array<Parsing_MIDI_Interface *, N> interfaces;
+};
