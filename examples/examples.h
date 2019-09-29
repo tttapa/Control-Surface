33134ed6c3c5044f0fc8680622031be9098c9636
Pieter Pas <pieter.pas@outlook.com>
2019-09-29 18:08:25 +0200

    Add CCButton example close #34

--- examples/examples.h
+++ examples/examples.h
@@ -157,6 +157,41 @@
  */
 
 /**
+ * @example   "CCButton.ino"
+ * 
+ * CCButton
+ * ========
+ * 
+ * This is an example that demonstrates the use of MIDI Control Change Buttons.
+ *
+ * @boards  AVR, AVR USB, Teensy 3.x, ESP32
+ * 
+ * Connections
+ * -----------
+ * 
+ * - 5: momentary push button (to ground)
+ * 
+ * The internal pull-up resistor for the button will be enabled automatically.
+ * 
+ * Behavior
+ * --------
+ * 
+ * - When the button on pin 5 is pressed, a MIDI Control Change message with a 
+ *   value of 0x7F (127) is sent for General Purpose Controller #1.
+ * - When the button on pin 5 is released, a MIDI Control Change message with a 
+ *   value of 0x00 (0) is sent for General Purpose Controller #1
+ * 
+ * Mapping
+ * -------
+ * 
+ * Select the Arduino as a custom MIDI controller in your DAW, and use the 
+ * MIDI learn option to assign the button to a function.
+ * 
+ * Written by PieterP, 2019-09-29  
+ * https://github.com/tttapa/Control-Surface
+ */
+
+/**
  * @example   "CCIncrementDecrementButtons.ino"
  * 
  * CCIncrementDecrementButtons
