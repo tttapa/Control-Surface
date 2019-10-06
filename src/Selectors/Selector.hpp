3223d48169e988570c916fb43b2bd66f75a07732
Pieter Pas <pieter.pas@outlook.com>
2019-10-06 16:42:56 +0200

    Selector doc

--- src/Selectors/Selector.hpp
+++ src/Selectors/Selector.hpp
@@ -20,6 +20,7 @@ enum class Wrap : bool {
     NoWrap = false,
 };
 
+/// A callback for the GenericSelector class that does nothing.
 struct EmptySelectorCallback {
     /// Initialize.
     void begin() {}
@@ -31,13 +32,44 @@ struct EmptySelectorCallback {
     }
 };
 
-template <setting_t N, class Callback = EmptySelectorCallback>
-class GenericSelector : public Updatable<> {
+/// Base class for all Selectors exposing the `get` method, so it can be used
+/// by display elements etc, without having to provide the full generic type.
+///
+/// A `set` method is not provided, because that would require either more
+/// virtual functions, or a rather large refactoring.
+class SelectorBase {
   protected:
+    /// Constructor.
+    SelectorBase() = default;
+
+  public:
+    /// Get the current selection/setting.
+    setting_t get() const { return setting; }
+
+  protected:
+    /// The selection of the selector. It is saved in the selector as well as
+    /// the selectable, because you need it in order to implement
+    /// increment/decrement methods.
+    setting_t setting = 0;
+};
+
+template <setting_t N, class Callback = EmptySelectorCallback>
+class GenericSelector : public SelectorBase, public Updatable<> {
+  public:
+    /**
+     * @brief   Constructor.
+     * 
+     * @param   selectable
+     *          The selectable object to manage. When the value of the selector
+     *          changes, it changes the selection of this selectable.
+     * @param   callback
+     *          The callback to call when the value changes. Used for (visual)
+     *          feedback from the selector (e.g. LEDs or some different kind of
+     *          display).
+     */
     GenericSelector(Selectable<N> &selectable, const Callback &callback)
         : selectable{selectable}, callback{callback} {}
 
-  public:
     void begin() override {
         callback.begin();
         reset();
@@ -45,12 +77,17 @@ class GenericSelector : public Updatable<> {
 
     void update() override { callback.update(); }
 
+    /// Reset the selection to the initial selection.
     void reset() { set(selectable.getInitialSelection()); }
 
-    setting_t get() const { return setting; }
-
+    /** 
+     * @brief   Select the given selection
+     * 
+     * @param   newSetting
+     *          The new setting to select [0, N-1].
+     */
     void set(setting_t newSetting) {
-        newSetting = Selectable<N>::validateSetting(newSetting);
+        newSetting = selectable.validateSetting(newSetting);
         selectable.select(newSetting);
         if (get() != newSetting) {
             callback.update(get(), newSetting);
@@ -58,6 +95,13 @@ class GenericSelector : public Updatable<> {
         }
     }
 
+    /**
+     * @brief   Add one to the setting, wrap around or clamp, depending on the
+     *          parameter, if the new setting would be out of range.
+     * 
+     * @param   wrap 
+     *          Wrap or clamp if the new setting would be out of range.
+     */
     void increment(Wrap wrap) {
         setting_t setting = this->get();
         setting++;
@@ -70,6 +114,13 @@ class GenericSelector : public Updatable<> {
         this->set(setting);
     }
 
+    /**
+     * @brief   Subtract one from the setting, wrap around or clamp, depending 
+     *          on the parameter, if the new setting would be out of range.
+     * 
+     * @param   wrap 
+     *          Wrap or clamp if the new setting would be out of range.
+     */
     void decrement(Wrap wrap) {
         setting_t setting = this->get();
         if (setting == 0) {
@@ -84,13 +135,21 @@ class GenericSelector : public Updatable<> {
 
   private:
     Selectable<N> &selectable;
+
+  public:
     Callback callback;
-    setting_t setting = 0;
 };
 
+/** 
+ * @brief   A Selector with an empty callback.
+ * 
+ * @tparam  N
+ *          The number of possible settings.
+ */
 template <setting_t N>
 class Selector : public GenericSelector<N> {
   public:
+    /// Constructor
     Selector(Selectable<N> &selectable) : GenericSelector<N>{selectable, {}} {}
 };
 
