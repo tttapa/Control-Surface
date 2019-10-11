b0a0a439af88aec07147a214fa41aa35e8c9523a
Pieter Pas <pieter.pas@outlook.com>
2019-10-12 01:37:55 +0200

    Add NoteRange tests

--- test/MIDI_Inputs/test-MIDINote.cpp
+++ test/MIDI_Inputs/test-MIDINote.cpp
@@ -60,6 +60,242 @@ TEST(NoteValue, resetAll) {
     EXPECT_FALSE(mn.getValue() > 0);
 }
 
+TEST(NoteRange, bankableInRange) {
+    Bank<2> bank(4);
+    Bankable::NoteRange<3, 2> mn = {bank, {0x10, CHANNEL_5}};
+
+    // First bank, first address
+    ChannelMessageMatcher midimsg1 = {0x90, CHANNEL_5, 0x10, 0x20};
+    EXPECT_TRUE(mn.updateWith(midimsg1));
+    EXPECT_EQ(mn.getValue(0), 0x20);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+
+    // First bank, third address
+    ChannelMessageMatcher midimsg2 = {0x90, CHANNEL_5, 0x10 + 2, 0x21};
+    EXPECT_TRUE(mn.updateWith(midimsg2));
+    EXPECT_EQ(mn.getValue(0), 0x20);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x21);
+
+    // Second bank, second address
+    ChannelMessageMatcher midimsg3 = {0x90, CHANNEL_5, 0x10 + 4 + 1, 0x22};
+    EXPECT_TRUE(mn.updateWith(midimsg3));
+    EXPECT_EQ(mn.getValue(0), 0x20);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x21);
+    bank.select(1);
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x22);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+
+    // First bank, first address
+    ChannelMessageMatcher midimsg4 = {0x90, CHANNEL_5, 0x10, 0x23};
+    EXPECT_TRUE(mn.updateWith(midimsg4));
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x22);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(0);
+    EXPECT_EQ(mn.getValue(0), 0x23);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x21);
+}
+
+TEST(NoteRange, bankableNotInRange) {
+    Bank<2> bank(4);
+    Bankable::NoteRange<3, 2> mn = {bank, {0x10, CHANNEL_5}};
+
+    // Before first bank
+    ChannelMessageMatcher midimsg1 = {0x90, CHANNEL_5, 0x10 - 1, 0x20};
+    EXPECT_FALSE(mn.updateWith(midimsg1));
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(1);
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(0);
+
+    // First bank, fourth address
+    ChannelMessageMatcher midimsg2 = {0x90, CHANNEL_5, 0x10 + 3, 0x20};
+    EXPECT_FALSE(mn.updateWith(midimsg2));
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(1);
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(0);
+
+    // Second bank, fourth address
+    ChannelMessageMatcher midimsg3 = {0x90, CHANNEL_5, 0x10 + 4 + 3, 0x20};
+    EXPECT_FALSE(mn.updateWith(midimsg3));
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(1);
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(0);
+
+    // Before first bank, first address
+    ChannelMessageMatcher midimsg4 = {0x90, CHANNEL_5, 0x10 - 4, 0x20};
+    EXPECT_FALSE(mn.updateWith(midimsg4));
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(1);
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(0);
+
+    // After last bank, first address
+    ChannelMessageMatcher midimsg5 = {0x90, CHANNEL_5, 0x10 + 8, 0x20};
+    EXPECT_FALSE(mn.updateWith(midimsg5));
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(1);
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(0);
+
+    // Different channel
+    ChannelMessageMatcher midimsg6 = {0x90, CHANNEL_5 + 1, 0x10, 0x20};
+    EXPECT_FALSE(mn.updateWith(midimsg6));
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(1);
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(0);
+}
+
+TEST(NoteRange, bankableInRangeChangeChannel) {
+    Bank<2> bank(4);
+    Bankable::NoteRange<3, 2> mn = {{bank, CHANGE_CHANNEL}, {0x10, CHANNEL_5}};
+
+    // First bank, first address
+    ChannelMessageMatcher midimsg1 = {0x90, CHANNEL_5, 0x10, 0x20};
+    EXPECT_TRUE(mn.updateWith(midimsg1));
+    EXPECT_EQ(mn.getValue(0), 0x20);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+
+    // First bank, third address
+    ChannelMessageMatcher midimsg2 = {0x90, CHANNEL_5, 0x10 + 2, 0x21};
+    EXPECT_TRUE(mn.updateWith(midimsg2));
+    EXPECT_EQ(mn.getValue(0), 0x20);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x21);
+
+    // Second bank, second address
+    ChannelMessageMatcher midimsg3 = {0x90, CHANNEL_5 + 4, 0x10 + 1, 0x22};
+    EXPECT_TRUE(mn.updateWith(midimsg3));
+    EXPECT_EQ(mn.getValue(0), 0x20);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x21);
+    bank.select(1);
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x22);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+
+    // First bank, first address
+    ChannelMessageMatcher midimsg4 = {0x90, CHANNEL_5, 0x10, 0x23};
+    EXPECT_TRUE(mn.updateWith(midimsg4));
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x22);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(0);
+    EXPECT_EQ(mn.getValue(0), 0x23);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x21);
+}
+
+TEST(NoteRange, bankableNotInRangeChangeChannel) {
+    Bank<2> bank(4);
+    Bankable::NoteRange<3, 2> mn = {{bank, CHANGE_CHANNEL}, {0x10, CHANNEL_5}};
+
+    // Before first bank
+    ChannelMessageMatcher midimsg1 = {0x90, CHANNEL_5, 0x10 - 1, 0x20};
+    EXPECT_FALSE(mn.updateWith(midimsg1));
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(1);
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(0);
+
+    // First bank, fourth address
+    ChannelMessageMatcher midimsg2 = {0x90, CHANNEL_5, 0x10 + 3, 0x20};
+    EXPECT_FALSE(mn.updateWith(midimsg2));
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(1);
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(0);
+
+    // Before second bank
+    ChannelMessageMatcher midimsg3 = {0x90, CHANNEL_5 + 4, 0x10 - 1, 0x20};
+    EXPECT_FALSE(mn.updateWith(midimsg3));
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(1);
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(0);
+
+    // Second bank, fourth address
+    ChannelMessageMatcher midimsg4 = {0x90, CHANNEL_5 + 4, 0x10 + 3, 0x20};
+    EXPECT_FALSE(mn.updateWith(midimsg4));
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(1);
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(0);
+
+    // Before first bank, first address
+    ChannelMessageMatcher midimsg5 = {0x90, CHANNEL_5 - 4, 0x10, 0x20};
+    EXPECT_FALSE(mn.updateWith(midimsg5));
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(1);
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(0);
+
+    // After last bank, first address
+    ChannelMessageMatcher midimsg6 = {0x90, CHANNEL_5 + 8, 0x10, 0x20};
+    EXPECT_FALSE(mn.updateWith(midimsg6));
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(1);
+    EXPECT_EQ(mn.getValue(0), 0x00);
+    EXPECT_EQ(mn.getValue(1), 0x00);
+    EXPECT_EQ(mn.getValue(2), 0x00);
+    bank.select(0);
+}
+
 #include <MIDI_Inputs/LEDs/NoteCCRangeLEDs.hpp>
 
 TEST(NoteValueLED, NoteOnNoteOff) {
