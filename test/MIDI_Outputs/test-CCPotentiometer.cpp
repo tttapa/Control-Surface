79b34e30daae44f10c231e8c34d0dc23c5d0bff8
Pieter Pas <pieter.pas@outlook.com>
2019-10-19 23:14:17 +0200

    Add mapping and inverting tests CCPotentiometer

--- test/MIDI_Outputs/test-CCPotentiometer.cpp
+++ test/MIDI_Outputs/test-CCPotentiometer.cpp
@@ -6,7 +6,7 @@
 using namespace ::testing;
 using namespace CS;
 
-TEST(CCPotentiometer, pressAndRelease) {
+TEST(CCPotentiometer, simple) {
     MockMIDI_Interface midi;
 
     CCPotentiometer pot(2, {0x3C, CHANNEL_7, 0xC});
@@ -21,6 +21,48 @@ TEST(CCPotentiometer, pressAndRelease) {
     EXPECT_CALL(midi, sendImpl(CC, 6, 0x3C, 28, 0xC));
     pot.update();
     EXPECT_CALL(midi, sendImpl(CC, 6, 0x3C, 37, 0xC));
+    pot.update();
+
+    Mock::VerifyAndClear(&ArduinoMock::getInstance());
+}
+
+TEST(CCPotentiometer, mapping) {
+    MockMIDI_Interface midi;
+
+    CCPotentiometer pot(2, {0x3C, CHANNEL_7, 0xC});
+    pot.begin();
+    pot.map([](analog_t x) -> analog_t { return x * 2; });
+
+    EXPECT_CALL(ArduinoMock::getInstance(), analogRead(2))
+        .Times(3)
+        .WillRepeatedly(Return(512));
+    InSequence s;
+    EXPECT_CALL(midi, sendImpl(CC, 6, 0x3C, 16 * 2, 0xC));
+    pot.update();
+    EXPECT_CALL(midi, sendImpl(CC, 6, 0x3C, 28 * 2, 0xC));
+    pot.update();
+    EXPECT_CALL(midi, sendImpl(CC, 6, 0x3C, 37 * 2, 0xC));
+    pot.update();
+
+    Mock::VerifyAndClear(&ArduinoMock::getInstance());
+}
+
+TEST(CCPotentiometer, invert) {
+    MockMIDI_Interface midi;
+
+    CCPotentiometer pot(2, {0x3C, CHANNEL_7, 0xC});
+    pot.begin();
+    pot.invert();
+
+    EXPECT_CALL(ArduinoMock::getInstance(), analogRead(2))
+        .Times(3)
+        .WillRepeatedly(Return(512));
+    InSequence s;
+    EXPECT_CALL(midi, sendImpl(CC, 6, 0x3C, 127 - 16, 0xC));
+    pot.update();
+    EXPECT_CALL(midi, sendImpl(CC, 6, 0x3C, 127 - 28, 0xC));
+    pot.update();
+    EXPECT_CALL(midi, sendImpl(CC, 6, 0x3C, 127 - 37, 0xC));
     pot.update();
 
     Mock::VerifyAndClear(&ArduinoMock::getInstance());
