6fd1baf13f5afff08b54bfb4b1138fa306a18226
Pieter Pas <pieter.pas@outlook.com>
2019-11-11 21:17:52 +0100

    Add arithmetic operations for Array

--- src/AH/Containers/Array.hpp
+++ src/AH/Containers/Array.hpp
@@ -3,12 +3,16 @@
 #pragma once
 
 #include <AH/Error/Error.hpp>
+#include <AH/STL/type_traits> // conditional
 #include <stddef.h> // size_t
 
 BEGIN_AH_NAMESPACE
 
 /// @addtogroup Containers
 /// @{
+
+template <class T, size_t N, size_t Start, size_t End, bool Const>
+class ArraySlice;
 
 /**
  * @brief   An array wrapper for easy copying, comparing, and iterating.
@@ -100,7 +104,269 @@ struct Array {
      *          The array to compare this array to.
      */
     bool operator!=(const Array<T, N> &rhs) const { return !(*this == rhs); }
+
+    template <size_t Start = 0, size_t End = N>
+    ArraySlice<T, N, Start, End, false> slice();
+
+    template <size_t Start = 0, size_t End = N>
+    ArraySlice<T, N, Start, End, true> slice() const;
+
+    template <size_t Start = 0, size_t End = N>
+    ArraySlice<T, N, Start, End, true> cslice() const {
+        const Array<T, N> &This = *this;
+        return This.slice();
+    }
 };
+
+template <class T, size_t N, size_t Start = 0, size_t End = N,
+          bool Const = true>
+class ArraySlice {
+    using ArrayRefType = typename std::conditional<Const, const Array<T, N> &,
+                                                   Array<T, N> &>::type;
+    using ElementRefType =
+        typename std::conditional<Const, const T &, T &>::type;
+
+  public:
+    ArraySlice(ArrayRefType array) : array{array} {}
+
+    explicit operator Array<T, End - Start>() const {
+        Array<T, End - Start> slice = {};
+        for (size_t i = 0; i < End - Start; ++i)
+            slice[i] = array[Start + i];
+        return slice;
+    }
+
+    /**
+     * @brief   Get the element at the given index.
+     * 
+     * @note    Bounds checking is performed. If fatal errors are disabled, the
+     *          last element is returned if the index is out of bounds. 
+     * 
+     * @param   index
+     *          The (zero-based) index of the element to return.
+     */
+    ElementRefType operator[](size_t index) const {
+        return array[Start + index];
+    }
+
+  private:
+    ArrayRefType &array;
+};
+
+template <class T, size_t N>
+template <size_t Start, size_t End>
+inline ArraySlice<T, N, Start, End, false> Array<T, N>::slice() {
+    return ArraySlice<T, N, Start, End, false>{*this};
+}
+
+template <class T, size_t N>
+template <size_t Start, size_t End>
+inline ArraySlice<T, N, Start, End, true> Array<T, N>::slice() const {
+    return ArraySlice<T, N, Start, End, true>{*this};
+}
+
+// Equality ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+/// Slice == Slice
+template <class T1, class T2, size_t N1, size_t N2, size_t Start1,
+          size_t Start2, size_t End1, size_t End2, bool Const1, bool Const2>
+bool operator==(ArraySlice<T1, N1, Start1, End1, Const1> a,
+                ArraySlice<T2, N2, Start2, End2, Const2> b) {
+    static_assert(End1 - Start1 == End2 - Start2, "Error: sizes do not match");
+    for (size_t i = 0; i < End1 - Start1; ++i)
+        if (a[i] != b[i])
+            return false;
+    return true;
+}
+
+/// Array == Slice
+template <class T1, class T2, size_t N1, size_t N2, size_t Start2, size_t End2,
+          bool Const2>
+bool operator==(const Array<T1, N1> &a,
+                ArraySlice<T2, N2, Start2, End2, Const2> b) {
+    return a.slice() == b;
+}
+
+/// Slice == Array
+template <class T1, class T2, size_t N1, size_t N2, size_t Start1, size_t End1,
+          bool Const1>
+bool operator==(ArraySlice<T2, N2, Start1, End1, Const1> a,
+                const Array<T1, N1> &b) {
+    return a == b.slice();
+}
+
+// Addition ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+/// Slice + Slice
+template <class T1, class T2, size_t N1, size_t N2, size_t Start1,
+          size_t Start2, size_t End1, size_t End2, bool Const1, bool Const2>
+Array<decltype(T1{} + T2{}), End1 - Start1>
+operator+(ArraySlice<T1, N1, Start1, End1, Const1> a,
+          ArraySlice<T2, N2, Start2, End2, Const2> b) {
+    static_assert(End1 - Start1 == End2 - Start2, "Error: sizes do not match");
+    Array<decltype(T1{} + T2{}), End1 - Start1> result = {};
+    for (size_t i = 0; i < End1 - Start1; ++i)
+        result[i] = a[i] + b[i];
+    return result;
+}
+
+/// Array + Array
+template <class T1, class T2, size_t N1, size_t N2>
+Array<decltype(T1{} + T2{}), N1> operator+(const Array<T1, N1> &a,
+                                           const Array<T2, N2> &b) {
+    return a.slice() + b.slice();
+}
+
+/// Slice += Slice
+template <class T1, class T2, size_t N1, size_t N2, size_t Start1,
+          size_t Start2, size_t End1, size_t End2, bool Const1, bool Const2>
+const ArraySlice<T1, N1, Start1, End1, Const1> &
+operator+=(const ArraySlice<T1, N1, Start1, End1, Const1> &a,
+           const ArraySlice<T2, N2, Start2, End2, Const2> &b) {
+    static_assert(End1 - Start1 == End2 - Start2, "Error: sizes do not match");
+    for (size_t i = 0; i < End1 - Start1; ++i)
+        a[i] += b[i];
+    return a;
+}
+
+/// Array += Array
+template <class T1, class T2, size_t N1, size_t N2>
+Array<T1, N1> &operator+=(Array<T1, N1> &a, const Array<T2, N2> &b) {
+    a.slice() += b.slice();
+    return a;
+}
+
+// Subtraction :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+/// Slice - Slice
+template <class T1, class T2, size_t N1, size_t N2, size_t Start1,
+          size_t Start2, size_t End1, size_t End2, bool Const1, bool Const2>
+Array<decltype(T1{} - T2{}), End1 - Start1>
+operator-(ArraySlice<T1, N1, Start1, End1, Const1> a,
+          ArraySlice<T2, N2, Start2, End2, Const2> b) {
+    static_assert(End1 - Start1 == End2 - Start2, "Error: sizes do not match");
+    Array<decltype(T1{} - T2{}), End1 - Start1> result = {};
+    for (size_t i = 0; i < End1 - Start1; ++i)
+        result[i] = a[i] - b[i];
+    return result;
+}
+
+/// Array - Array
+template <class T1, class T2, size_t N1, size_t N2>
+Array<decltype(T1{} - T2{}), N1> operator-(const Array<T1, N1> &a,
+                                           const Array<T2, N2> &b) {
+    return a.slice() - b.slice();
+}
+
+/// Slice -= Slice
+template <class T1, class T2, size_t N1, size_t N2, size_t Start1,
+          size_t Start2, size_t End1, size_t End2, bool Const1, bool Const2>
+const ArraySlice<T1, N1, Start1, End1, Const1> &
+operator-=(const ArraySlice<T1, N1, Start1, End1, Const1> &a,
+           const ArraySlice<T2, N2, Start2, End2, Const2> &b) {
+    static_assert(End1 - Start1 == End2 - Start2, "Error: sizes do not match");
+    for (size_t i = 0; i < End1 - Start1; ++i)
+        a[i] -= b[i];
+    return a;
+}
+
+/// Array -= Array
+template <class T1, class T2, size_t N1, size_t N2>
+Array<T1, N1> &operator-=(Array<T1, N1> &a, const Array<T2, N2> &b) {
+    a.slice() -= b.slice();
+    return a;
+}
+
+// Scalar Multiplication :::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+/// Slice * Scalar
+template <class T1, class T2, size_t N1, size_t Start1, size_t End1,
+          bool Const1>
+Array<decltype(T1{} * T2{}), End1 - Start1>
+operator*(ArraySlice<T1, N1, Start1, End1, Const1> a, T2 b) {
+    Array<decltype(T1{} * T2{}), End1 - Start1> result = {};
+    for (size_t i = 0; i < End1 - Start1; ++i)
+        result[i] = a[i] * b;
+    return result;
+}
+
+/// Array * Scalar
+template <class T1, class T2, size_t N1>
+Array<decltype(T1{} * T2{}), N1> operator*(const Array<T1, N1> &a, T2 b) {
+    return a.slice() * b;
+}
+
+/// Scalar * Slice
+template <class T1, class T2, size_t N2, size_t Start2, size_t End2,
+          bool Const2>
+Array<decltype(T1{} * T2{}), End2 - Start2>
+operator*(T1 a, ArraySlice<T2, N2, Start2, End2, Const2> b) {
+    Array<decltype(T1{} * T2{}), End2 - Start2> result = {};
+    for (size_t i = 0; i < End2 - Start2; ++i)
+        result[i] = a * b[i];
+    return result;
+}
+
+/// Scalar * Array
+template <class T1, class T2, size_t N2>
+Array<decltype(T1{} * T2{}), N2> operator*(T1 a, const Array<T2, N2> &b) {
+    return a * b.slice();
+}
+
+/// Slice *= Scalar
+template <class T1, class T2, size_t N1, size_t Start1, size_t End1,
+          bool Const1>
+const ArraySlice<T1, N1, Start1, End1, Const1> &
+operator*=(const ArraySlice<T1, N1, Start1, End1, Const1> &a, T2 b) {
+    for (size_t i = 0; i < End1 - Start1; ++i)
+        a[i] *= b;
+    return a;
+}
+
+/// Array *= Scalar
+template <class T1, class T2, size_t N1>
+Array<T1, N1> &operator*=(Array<T1, N1> &a, T2 b) {
+    a.slice() *= b;
+    return a;
+}
+
+// Scalar Division :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+/// Slice / Scalar
+template <class T1, class T2, size_t N1, size_t Start1, size_t End1,
+          bool Const1>
+Array<decltype(T1{} / T2{}), End1 - Start1>
+operator/(ArraySlice<T1, N1, Start1, End1, Const1> a, T2 b) {
+    Array<decltype(T1{} / T2{}), End1 - Start1> result = {};
+    for (size_t i = 0; i < End1 - Start1; ++i)
+        result[i] = a[i] / b;
+    return result;
+}
+
+/// Array / Scalar
+template <class T1, class T2, size_t N1>
+Array<decltype(T1{} / T2{}), N1> operator/(const Array<T1, N1> &a, T2 b) {
+    return a.slice() / b;
+}
+
+/// Slice /= Scalar
+template <class T1, class T2, size_t N1, size_t Start1, size_t End1,
+          bool Const1>
+const ArraySlice<T1, N1, Start1, End1, Const1> &
+operator/=(const ArraySlice<T1, N1, Start1, End1, Const1> &a, T2 b) {
+    for (size_t i = 0; i < End1 - Start1; ++i)
+        a[i] /= b;
+    return a;
+}
+
+/// Array /= Scalar
+template <class T1, class T2, size_t N1>
+Array<T1, N1> &operator/=(Array<T1, N1> &a, T2 b) {
+    a.slice() /= b;
+    return a;
+}
+
+// Type aliases ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 /// An easy alias for two-dimensional Arrays.
 template <class T, size_t nb_rows, size_t nb_cols>
--- test/AH/Containers/test-Array.cpp
+++ test/AH/Containers/test-Array.cpp
@@ -58,6 +58,131 @@ TEST(Array, outOfBounds) {
     EXPECT_THROW(b[5], AH::ErrorException);
 }
 
+// Addition
+
+TEST(Array, sliceAdd) {
+    Array<int, 5> a = {1, 2, 3, 4, 5};
+    Array<int, 5> b = {1, 2, 3, 4, 5};
+    Array<int, 3> c = {4, 6, 8};
+    auto s1 = a.slice<0, 3>();
+    auto s2 = b.slice<2, 5>();
+    EXPECT_EQ(s1 + s2, c);
+}
+
+TEST(Array, add) {
+    Array<int, 3> a = {3, 4, 5};
+    Array<int, 3> b = {1, 2, 3};
+    Array<int, 3> c = {4, 6, 8};
+    EXPECT_EQ(a + b, c);
+}
+
+TEST(Array, sliceCompAdd) {
+    Array<int, 5> a = {1, 2, 3, 4, 5};
+    Array<int, 5> b = {1, 2, 3, 4, 5};
+    Array<int, 5> c = {4, 6, 8, 4, 5};
+    auto s1 = a.slice<0, 3>();
+    auto s2 = b.slice<2, 5>();
+    auto s3 = c.slice<0, 3>();
+    auto s4 = s1 += s2;
+    EXPECT_EQ(s4, s3);
+    EXPECT_EQ(a, c);
+}
+
+TEST(Array, compAdd) {
+    Array<int, 3> a = {3, 4, 5};
+    Array<int, 3> b = {1, 2, 3};
+    Array<int, 3> c = {4, 6, 8};
+    auto d = a += b;
+    EXPECT_EQ(d, c);
+    EXPECT_EQ(a, c);
+}
+
+// Subtraction
+
+TEST(Array, sliceSub) {
+    Array<int, 5> a = {1, 2, 3, 4, 5};
+    Array<int, 5> b = {1, 2, 4, 8, 16};
+    Array<int, 3> c = {-3, -6, -13};
+    auto s1 = a.slice<0, 3>();
+    auto s2 = b.slice<2, 5>();
+    EXPECT_EQ(s1 - s2, c);
+}
+
+TEST(Array, sub) {
+    Array<int, 3> a = {1, 2, 3};
+    Array<int, 3> b = {4, 8, 16};
+    Array<int, 3> c = {-3, -6, -13};
+    EXPECT_EQ(a - b, c);
+}
+
+// Scalar multiplication
+
+TEST(Array, sliceMult) {
+    Array<int, 5> a = {1, 2, 3, 4, 5};
+    Array<int, 3> b = {4, 6, 8};
+    auto s1 = a.slice<1, 4>();
+    EXPECT_EQ(s1 * 2, b);
+    EXPECT_EQ(2 * s1, b);
+}
+
+TEST(Array, mult) {
+    Array<int, 5> a = {1, 2, 3, 4, 5};
+    Array<int, 5> b = {2, 4, 6, 8, 10};
+    EXPECT_EQ(a * 2, b);
+    EXPECT_EQ(2 * a, b);
+}
+
+TEST(Array, sliceCompMult) {
+    Array<int, 5> a = {1, 2, 3, 4, 5};
+    Array<int, 5> b = {1, 4, 6, 8, 5};
+    auto s1 = a.slice<1, 4>();
+    auto c = s1 *= 2;
+    auto s3 = b.slice<1, 4>();
+    EXPECT_EQ(a, b);
+    EXPECT_EQ(c, s3);
+}
+
+TEST(Array, compMult) {
+    Array<int, 5> a = {1, 2, 3, 4, 5};
+    Array<int, 5> b = {2, 4, 6, 8, 10};
+    auto c = a *= 2;
+    EXPECT_EQ(a, b);
+    EXPECT_EQ(c, b);
+}
+
+// Scalar division
+
+TEST(Array, sliceDiv) {
+    Array<int, 5> a = {2, 4, 6, 8, 10};
+    Array<int, 3> b = {2, 3, 4};
+    auto s1 = a.slice<1, 4>();
+    EXPECT_EQ(s1 / 2, b);
+}
+
+TEST(Array, div) {
+    Array<int, 5> a = {2, 4, 6, 8, 10};
+    Array<int, 5> b = {1, 2, 3, 4, 5};
+    EXPECT_EQ(a / 2, b);
+}
+
+TEST(Array, sliceCompDiv) {
+    Array<int, 5> a = {1, 4, 6, 8, 5};
+    Array<int, 5> b = {1, 2, 3, 4, 5};
+    auto s1 = a.slice<1, 4>();
+    auto c = s1 /= 2;
+    auto s3 = b.slice<1, 4>();
+    EXPECT_EQ(a, b);
+    EXPECT_EQ(c, s3);
+}
+
+TEST(Array, compDiv) {
+    Array<int, 5> a = {2, 4, 6, 8, 10};
+    Array<int, 5> b = {1, 2, 3, 4, 5};
+    auto c = a /= 2;
+    EXPECT_EQ(a, b);
+    EXPECT_EQ(c, b);
+}
+
 // -------------------------------------------------------------------------- //
 
 TEST(Array2D, initialize) {
