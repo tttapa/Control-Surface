1caabd7b72aad26a679562e7ceb834dae710c688
Pieter Pas <pieter.pas@outlook.com>
2019-11-12 12:38:22 +0100

    STL Test cmath

--- examples/test/STL-test/STL-test.ino
+++ examples/test/STL-test/STL-test.ino
@@ -2,7 +2,6 @@
  * @boards  AVR, AVR USB
  */
 
-
 #include <Arduino_Helpers.h>
 
 #include <AH/STL/algorithm>
@@ -10,10 +9,64 @@
 #include <AH/STL/cstdint>
 #include <AH/STL/cstdlib>
 #include <AH/STL/initializer_list>
-#include <AH/STL/utility>
-#include <AH/STL/new>
 #include <AH/STL/limits>
+#include <AH/STL/new>
 #include <AH/STL/type_traits>
+#include <AH/STL/utility>
 
-void setup() {}
-void loop() {}
+using std::is_same;
+
+#define TEST_CMATH_FUNCTION(Fun)                                               \
+  static_assert(is_same<decltype(Fun((float)1)), float>::value,                \
+                "cmath error " #Fun "(float)");                                \
+  static_assert(is_same<decltype(Fun((double)1)), double>::value,              \
+                "cmath error " #Fun "(double)");                               \
+  static_assert(is_same<decltype(Fun((long double)1)), long double>::value,    \
+                "cmath error " #Fun "(long double)");
+
+#define TEST_CMATH_FUNCTION_I(Fun)                                             \
+  TEST_CMATH_FUNCTION(Fun);                                                    \
+  static_assert(is_same<decltype(Fun((int)1)), int>::value,                    \
+                "cmath error " #Fun "(int)");
+
+#define TEST_CMATH_FUNCTION3(Fun)                                              \
+  static_assert(                                                               \
+    is_same<decltype(Fun((float)1, (float)1, (float)1)), float>::value,        \
+    "cmath error " #Fun "(float)");                                            \
+  static_assert(                                                               \
+    is_same<decltype(Fun((double)1, (double)1, (double)1)), double>::value,    \
+    "cmath error " #Fun "(double)");                                           \
+  static_assert(                                                               \
+    is_same<decltype(Fun((long double)1, (long double)1, (long double)1)),     \
+            long double>::value,                                               \
+    "cmath error " #Fun "(long double)");
+
+#define TEST_CMATH_FUNCTION2(Fun)                                             \
+  static_assert(is_same<decltype(Fun((float)1, (float)1)), float>::value,      \
+                "cmath error " #Fun "(float)");                                \
+  static_assert(is_same<decltype(Fun((double)1, (double)1)), double>::value,   \
+                "cmath error " #Fun "(double)");                               \
+  static_assert(is_same<decltype(Fun((long double)1, (long double)1)),         \
+                        long double>::value,                                   \
+                "cmath error " #Fun "(long double)");
+
+TEST_CMATH_FUNCTION(std::round);
+TEST_CMATH_FUNCTION(std::sin);
+TEST_CMATH_FUNCTION(std::cos);
+TEST_CMATH_FUNCTION(std::tan);
+TEST_CMATH_FUNCTION(std::asin);
+TEST_CMATH_FUNCTION(std::acos);
+TEST_CMATH_FUNCTION(std::atan);
+TEST_CMATH_FUNCTION(std::exp);
+TEST_CMATH_FUNCTION(std::log);
+TEST_CMATH_FUNCTION(std::log10);
+
+TEST_CMATH_FUNCTION_I(std::abs);
+
+TEST_CMATH_FUNCTION2(std::hypot);
+TEST_CMATH_FUNCTION3(std::fma);
+
+void setup() {
+}
+void loop() {
+}
