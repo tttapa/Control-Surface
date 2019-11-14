790d7aab1e81feb90e1b23bc71199c317ab8d9aa
Pieter Pas <pieter.pas@outlook.com>
2019-11-14 17:33:59 +0100

    Rename CI scripts

--- scripts/build-ci.sh
+++ scripts/build-ci.sh
@@ -3,8 +3,8 @@
 set -e
 
 export PATH="$HOME/.local/bin:$PATH"
-cd "$TRAVIS_BUILD_DIR/build"
+cd "${TRAVIS_BUILD_DIR}${GITHUB_WORKSPACE}/build"
 cmake .. -DFAST_COMPILE=ON
 make -j4
 make test
-make arduino_travis
+make arduino_ci
