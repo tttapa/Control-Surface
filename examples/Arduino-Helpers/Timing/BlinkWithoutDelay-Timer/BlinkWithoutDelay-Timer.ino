0bf5f2d2f2d2b1846fa8fa1917c8ee144235de27
Pieter Pas <pieter.pas@outlook.com>
2019-12-06 00:18:41 +0100

    Add timer example

---
+++ examples/Arduino-Helpers/Timing/BlinkWithoutDelay-Timer/BlinkWithoutDelay-Timer.ino
@@ -0,0 +1,29 @@
+/**
+ * This examples shows how use the Timer class to blink an LED every
+ * second, in a non-blocking fashion (without using delay).
+ * 
+ * @boards  AVR, AVR USB, Nano 33, Due, Teensy 3.x, ESP8266, ESP32
+ * 
+ * Behavior
+ * --------
+ * 
+ * Blinks the built-in LED every second.
+ * 
+ * Written by PieterP, 2019-12-06  
+ * https://github.com/tttapa/Arduino-Helpers
+ */
+
+#include <Arduino_Helpers.h>
+#include <AH/Timing/MillisMicrosTimer.hpp>
+
+Timer<millis> timer = 500; // milliseconds
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  if (timer) {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  }
+}
