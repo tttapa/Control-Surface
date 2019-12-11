93839840483665bb81b73f58c625030874bda5af
Pieter Pas <pieter.pas@outlook.com>
2019-12-11 21:01:58 +0100

    Add example for #74

---
+++ examples/Other/GitHub-issues/Transpose-Octave-NC-Button/Transpose-Octave-NC-Button.ino
@@ -0,0 +1,30 @@
+/**
+ * <https://github.com/tttapa/Control-Surface/issues/74>
+ */
+
+#include <Control_Surface.h>
+
+USBMIDI_Interface midi;
+
+// Transposer that transposes one octave (12 semitones) at a time,
+// from 0 to +1 octave.
+Transposer<0, +1> transposer(12);
+
+// Push button on pin 2 to transpose.
+IncrementSelector<2> selector = {transposer, 2};
+
+using namespace MIDI_Notes;
+Bankable::NoteButton notebutton = {
+  transposer, // bank/transposer
+  3,          // pin
+  note(C, 4), // address/note
+};
+
+void setup() {
+  selector.invert(); // normally closed button
+  Control_Surface.begin();
+}
+
+void loop() {
+  Control_Surface.loop();
+}
