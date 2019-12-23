0ec8eac68b8362178c8f3eeeeb158b8ab967f2b9
Pieter Pas <pieter.pas@outlook.com>
2019-12-23 21:33:18 +0100

    MIDI Pipes working

--- src/MIDI_Interfaces/MIDI_Pipes.cpp
+++ src/MIDI_Interfaces/MIDI_Pipes.cpp
@@ -7,39 +7,43 @@ BEGIN_CS_NAMESPACE
 
 // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: //
 
-bool MIDI_Sink::connectSourcePipe(MIDI_Pipe *source) {
-    if (this->sourcePipe != nullptr) {
-        ERROR("MIDI_Sink already has a pipe connected to it", 0x9412);
-        return false;
+void MIDI_Sink::connectSourcePipe(MIDI_Pipe *source) {
+    if (this->sourcePipe == nullptr) {
+        source->connectSink(this);
+        this->sourcePipe = source;
+    } else {
+        this->sourcePipe->connectSourcePipe(source);
     }
-    this->sourcePipe = source;
-    return true;
 }
 
-void MIDI_Sink::disconnectSourcePipe() { this->sourcePipe = nullptr; }
-
-MIDI_Sink::~MIDI_Sink() {
-    if (sourcePipe != nullptr)
+void MIDI_Sink::disconnectSourcePipe() {
+    if (sourcePipe != nullptr) {
         sourcePipe->disconnectSink();
+        sourcePipe = nullptr;
+    }
 }
+
+MIDI_Sink::~MIDI_Sink() { disconnectSourcePipe(); }
 
 // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: //
 
-bool MIDI_Source::connectSinkPipe(MIDI_Pipe *sink) {
-    if (this->sinkPipe != nullptr) {
-        ERROR("MIDI_Sink already has a pipe connected to it", 0x9412);
-        return false;
+void MIDI_Source::connectSinkPipe(MIDI_Pipe *sink) {
+    if (this->sinkPipe == nullptr) {
+        sink->connectSource(this);
+        this->sinkPipe = sink;
+    } else {
+        this->sinkPipe->connectSinkPipe(sink);
     }
-    this->sinkPipe = sink;
-    return true;
 }
 
-void MIDI_Source::disconnectSinkPipe() { this->sinkPipe = nullptr; }
-
-MIDI_Source::~MIDI_Source() {
-    if (sinkPipe != nullptr)
+void MIDI_Source::disconnectSinkPipe() {
+    if (sinkPipe != nullptr) {
         sinkPipe->disconnectSource();
+        sinkPipe = nullptr;
+    }
 }
+
+MIDI_Source::~MIDI_Source() { disconnectSinkPipe(); }
 
 void MIDI_Source::sendMIDI(ChannelMessage msg) {
     if (sinkPipe != nullptr)
@@ -57,41 +61,41 @@ void MIDI_Source::sendMIDI(RealTimeMessage msg) {
 // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: //
 
 void MIDI_Pipe::connectSink(MIDI_Sink *sink) {
-    if (hasSink())
-        this->sink->disconnectSourcePipe();
-    if (sink->connectSourcePipe(this))
-        this->sink = sink;
+    assert(this->sink == nullptr);
+    this->sink = sink;
 }
 
-void MIDI_Pipe::disconnectSink() {
-    if (hasSink())
-        this->sink->disconnectSourcePipe();
-}
+void MIDI_Pipe::disconnectSink() { this->sink = nullptr; }
 
 void MIDI_Pipe::connectSource(MIDI_Source *source) {
-    if (hasSource())
-        this->source->disconnectSinkPipe();
-    if (source->connectSinkPipe(this))
-        this->sink = sink;
+    assert(this->source == nullptr);
+    this->source = source;
 }
 
-void MIDI_Pipe::disconnectSource() {
-    if (hasSource())
-        this->source->disconnectSinkPipe();
-}
+void MIDI_Pipe::disconnectSource() { this->source = nullptr; }
 
-MIDI_Pipe::~MIDI_Pipe() {
+void MIDI_Pipe::disconnect() {
     if (hasSink() && hasThroughIn()) {
-        throughIn->connectSink(sink);
+        auto oldSink = sink;
         sink->disconnectSourcePipe();
-        sink->connectSourcePipe(throughIn);
+        throughIn->disconnectSink();
+        oldSink->connectSourcePipe(throughIn);
     }
     if (hasSource() && hasThroughOut()) {
-        throughOut->connectSource(source);
+        auto oldSource = source;
         source->disconnectSinkPipe();
-        source->connectSinkPipe(throughIn);
+        throughOut->disconnectSource();
+        oldSource->connectSinkPipe(throughOut);
+    }
+    if (hasSink()) {
+        sink->disconnectSourcePipe();
+    }
+    if (hasSource()) {
+        source->disconnectSinkPipe();
     }
 }
+
+MIDI_Pipe::~MIDI_Pipe() { disconnect(); }
 
 END_CS_NAMESPACE
 
