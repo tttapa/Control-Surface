3f640698fd4675a4d4fdd75d1003c8e0a387360c
Pieter Pas <pieter.pas@outlook.com>
2020-05-10 12:31:46 +0200

    Fix MIDI USB name for AVR Teensies

--- src/MIDI_Interfaces/Teensy-USB-MIDI-name.c
+++ src/MIDI_Interfaces/Teensy-USB-MIDI-name.c
@@ -3,7 +3,7 @@
 //                                                                            //
 // Edit Teensy-USB-MIDI-name.py instead.                                      //
 
-#if defined(ARDUINO) && defined(TEENSYDUINO) && !defined(__AVR_AT90USB1286__)
+#if defined(ARDUINO) && defined(TEENSYDUINO) && !defined(AVR)
 #include "usb_names.h"
 
 /// USB name: "Control Surface"
