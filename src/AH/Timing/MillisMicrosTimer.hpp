a7af51788f320bb834bc8c404145ab1a81d65467
Pieter Pas <pieter.pas@outlook.com>
2020-06-19 14:00:40 +0200

    Add Timer::beginNextPeriod()

--- src/AH/Timing/MillisMicrosTimer.hpp
+++ src/AH/Timing/MillisMicrosTimer.hpp
@@ -21,8 +21,8 @@ using timefunction = unsigned long (*)();
  * @brief   A class for easily managing timed events. A wrapper for "Blink 
  *          Without Delay".
  * 
- * @tparam time
- *         The time function to use.
+ * @tparam  time
+ *          The time function to use.
  */
 template <timefunction time = micros>
 class Timer {
@@ -37,8 +37,10 @@ class Timer {
         begin();
 #endif
     }
-    /// Initialize the timer.
+    /// Initialize or reset the timer. The timer will fire immediately.
     void begin() { previous = time() - interval; }
+    /// Initialize or reset the timer. The timer will fire after one period.
+    void beginNextPeriod() { previous = time(); }
     /// Update the timer and return true if the event should fire.
     explicit operator bool() {
         auto now = time();
