07f107e1c5b8a74d8130071ed59b242fd36e2416
Pieter Pas <pieter.pas@outlook.com>
2022-05-21 18:37:04 +0200

    [Docs] update examples.py

--- scripts/ci/gen-docs.sh
+++ scripts/ci/gen-docs.sh
@@ -24,6 +24,7 @@ function run_doxygen_coverage {
         echo "GENERATE_LATEX = NO" 
     ) > tmp-Doxyfile
     # Generate the documentation
+    python3 scripts/examples.py
     doxygen tmp-Doxyfile
     rm -rf tmp-Doxyfile
     popd
