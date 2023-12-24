e9c06a2a47000f7f9db4b3fc86f9e5b3bf88cca1
Pieter Pas <pieter.pas@outlook.com>
2023-12-24 20:08:44 +0100

    Improve warnings

--- src/AH/Settings/Warnings.hpp
+++ src/AH/Settings/Warnings.hpp
@@ -7,7 +7,8 @@
 #define AH_DIAGNOSTIC_WERROR()                                                 \
     _Pragma("GCC diagnostic push") _Pragma("GCC diagnostic error \"-Wall\"")   \
         _Pragma("GCC diagnostic error \"-Wextra\"")                            \
-            _Pragma("GCC diagnostic ignored \"-Wc++0x-compat\"")
+            _Pragma("GCC diagnostic ignored \"-Wc++0x-compat\"")               \
+                _Pragma("GCC diagnostic ignored \"-Wc++11-compat\"")
 #define AH_DIAGNOSTIC_POP() _Pragma("GCC diagnostic pop")
 #define AH_DIAGNOSTIC_EXTERNAL_HEADER()                                        \
     _Pragma("GCC diagnostic push") _Pragma("GCC diagnostic ignored \"-Wall\"") \
@@ -34,15 +35,12 @@
 #else // __GNUC__ < 5
 
 #define AH_DIAGNOSTIC_WERROR()                                                 \
-    _Pragma("GCC diagnostic push") _Pragma("GCC diagnostic error \"-Wall\"")   \
-        _Pragma("GCC diagnostic error \"-Wextra\"")                            \
-            _Pragma("GCC diagnostic ignored \"-Wc++0x-compat\"")               \
-                _Pragma("GCC diagnostic ignored \"-Wattributes\"")
+    _Pragma("GCC diagnostic push")                                             \
+        _Pragma("GCC diagnostic ignored \"-Wattributes\"")
 #define AH_DIAGNOSTIC_POP() _Pragma("GCC diagnostic pop")
 #define AH_DIAGNOSTIC_EXTERNAL_HEADER()                                        \
-    _Pragma("GCC diagnostic push") _Pragma("GCC diagnostic ignored \"-Wall\"") \
-        _Pragma("GCC diagnostic ignored \"-Wextra\"")                          \
-            _Pragma("GCC diagnostic ignored \"-Wunused-parameter\"")
+    _Pragma("GCC diagnostic push")                                             \
+        _Pragma("GCC diagnostic ignored \"-Wunused-parameter\"")
 
 #endif
 
