7419b6cf6eef3d700454ffc1f102db5aae72a00d
Pieter Pas <pieter.pas@outlook.com>
2024-01-19 00:58:20 +0100

    Improve ESP32 NimBLE support (WIP)

--- src/MIDI_Interfaces/GenericBLEMIDI_Interface.hpp
+++ src/MIDI_Interfaces/GenericBLEMIDI_Interface.hpp
@@ -16,9 +16,84 @@
 
 BEGIN_CS_NAMESPACE
 
+class MIDIBLEParser {
+  public:
+    /// Parse the given BLE data immediately, in a thread-safe way, and push the
+    /// results to the MIDI message queue.
+    void parseNowThreadSafe(BLEDataGenerator ble_data) {
+        auto chunk = ble_data();
+        if (chunk.length == 0)
+            return;
+        ble_parser = {chunk.data, chunk.length};
+        while (true) {
+            MIDIReadEvent event = parser.pull(ble_parser);
+            while (event != MIDIReadEvent::NO_MESSAGE) {
+                switch (event) {
+                    case MIDIReadEvent::CHANNEL_MESSAGE:
+                        if (!queue.push(parser.getChannelMessage(),
+                                        ble_parser.getTimestamp()))
+                            queueFull();
+                        break;
+                    case MIDIReadEvent::SYSEX_CHUNK: // fallthrough
+                    case MIDIReadEvent::SYSEX_MESSAGE:
+                        if (!queue.push(parser.getSysExMessage(),
+                                        ble_parser.getTimestamp()))
+                            queueFull();
+                        break;
+                    case MIDIReadEvent::REALTIME_MESSAGE:
+                        if (!queue.push(parser.getRealTimeMessage(),
+                                        ble_parser.getTimestamp()))
+                            queueFull();
+                        break;
+                    case MIDIReadEvent::SYSCOMMON_MESSAGE:
+                        if (!queue.push(parser.getSysCommonMessage(),
+                                        ble_parser.getTimestamp()))
+                            queueFull();
+                        break;
+                    case MIDIReadEvent::NO_MESSAGE: break; // LCOV_EXCL_LINE
+                    default: break;                        // LCOV_EXCL_LINE
+                }
+                event = parser.pull(ble_parser);
+            }
+            chunk = ble_data();
+            if (chunk.length == 0)
+                break;
+            ble_parser.extend(chunk.data, chunk.length);
+        }
+        parser.cancelRunningStatus();
+    }
+
+    using IncomingMIDIMessage = MIDIMessageQueue::MIDIMessageQueueElement;
+
+    bool popMessage(IncomingMIDIMessage &incomingMessage) {
+        return queue.pop(incomingMessage);
+    }
+
+    unsigned getNumDroppedMessages() const {
+        return queue_full_count.load(std::memory_order_relaxed);
+    }
+
+  private:
+    /// May contain incoming data to be parsed.
+    BLEDataGenerator ble_data {};
+    /// Parses the (chunked) BLE packet obtained from @ref readdata.
+    BLEMIDIParser ble_parser {nullptr, 0};
+    /// MIDI Parser for incoming data.
+    SerialMIDI_Parser parser {false};
+    /// Queue for incoming MIDI messages.
+    MIDIMessageQueue queue {64};
+    /// The number of times a MIDI message was dropped because the queue was
+    /// full.
+    std::atomic_uint queue_full_count {0};
+
+    void queueFull() {
+        queue_full_count.fetch_add(1, std::memory_order_relaxed);
+    }
+};
+
 // TODO: move to separate file
-struct MIDIBLEBackend : private FreeRTOSBLEMIDISender<MIDIBLEBackend>,
-                        private MIDIBLEInstance {
+class MIDIBLEBackend : private FreeRTOSBLEMIDISender<MIDIBLEBackend>,
+                       private MIDIBLEInstance {
   private:
     using Sender = FreeRTOSBLEMIDISender<MIDIBLEBackend>;
     friend Sender;
@@ -63,6 +138,16 @@ struct MIDIBLEBackend : private FreeRTOSBLEMIDISender<MIDIBLEBackend>,
     void handleData(BLEConnectionHandle conn_handle, BLEDataGenerator &&data,
                     BLEDataLifetime lifetime) override {
         ESP_LOGI("CS-BLEMIDI", "conn=%d", conn_handle.conn);
+        parser.parseNowThreadSafe(std::move(data));
+    }
+
+  private:
+    MIDIBLEParser parser;
+
+  public:
+    using IncomingMIDIMessage = MIDIBLEParser::IncomingMIDIMessage;
+    bool popMessage(IncomingMIDIMessage &incomingMessage) {
+        return parser.popMessage(incomingMessage);
     }
 
   public:
@@ -142,34 +227,13 @@ class GenericBLEMIDI_Interface : public MIDI_Interface {
     void sendSysExImpl(SysExMessage) override;
     void sendRealTimeImpl(RealTimeMessage) override;
     /// Send the buffered MIDI BLE packet immediately.
-    void sendNowImpl() override {
-        auto lck = backend.acquirePacket();
-        backend.sendNow(lck);
-    }
-
-  public:
-    /// Parse the given BLE data immediately, in a thread-safe way, and push the
-    /// results to the MIDI message queue.
-    void parseNowThreadSafe(BLEDataGenerator ble_data);
+    void sendNowImpl() override;
 
   private:
-    /// May contain incoming data to be parsed.
-    BLEDataGenerator ble_data {};
-    /// Parses the (chunked) BLE packet obtained from @ref readdata.
-    BLEMIDIParser ble_parser {nullptr, 0};
-    /// MIDI Parser for incoming data.
-    SerialMIDI_Parser parser {false};
-    /// Queue for incoming MIDI messages.
-    MIDIMessageQueue queue {64};
     /// Incoming message that can be from retrieved using the
     /// `getChannelMessage()`, `getSysCommonMessage()`, `getRealTimeMessage()`
     /// and `getSysExMessage()` methods.
-    MIDIMessageQueue::MIDIMessageQueueElement incomingMessage;
-    std::atomic_uint queue_full_count {0};
-
-    void queueFull() {
-        queue_full_count.fetch_add(1, std::memory_order_relaxed);
-    }
+    Backend::IncomingMIDIMessage incomingMessage;
 };
 
 END_CS_NAMESPACE
