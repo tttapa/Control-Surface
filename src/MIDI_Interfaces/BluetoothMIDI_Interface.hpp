052a3d257304a2f0673efe952b510f128829fa76
Pieter Pas <pieter.pas@outlook.com>
2024-01-20 17:54:04 +0100

    Add BluetoothMIDI_Interface with sensible defaults

---
+++ src/MIDI_Interfaces/BluetoothMIDI_Interface.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#ifdef ESP32
+#include <sdkconfig.h>
+#if CONFIG_BT_BLE_ENABLED
+// ESP32 with BLE support
+#ifdef CS_USE_NIMBLE
+// NimBLE backend
+#include "BLEMIDI/ESP32NimBLEBackend.hpp"
+BEGIN_CS_NAMESPACE
+using BLEMIDIBackend = ESP32NimBLEBackend;
+END_CS_NAMESPACE
+#define CS_BLEMIDI_SUPPORTED 1
+#else
+// Bluedroid backend (default)
+#include "BLEMIDI/ESP32BluedroidBackend.hpp"
+BEGIN_CS_NAMESPACE
+using BLEMIDIBackend = ESP32BluedroidBackend;
+END_CS_NAMESPACE
+#define CS_BLEMIDI_SUPPORTED 1
+#endif
+#endif
+#elif (defined(ARDUINO_ARCH_MBED) && defined(ARDUINO_ARDUINO_NANO33BLE)) ||    \
+    (defined(ARDUINO_ARCH_MBED) && defined(ARDUINO_ARCH_RP2040)) ||            \
+    defined(ARDUINO_UNOR4_WIFI) || defined(ARDUINO_SAMD_NANO_33_IOT)
+// Arduino Nano 33 BLE, RP2040, Arduino UNO R4 WiFi, Arduino Nano 33 IoT
+#include "BLEMIDI/ArduinoBLEBackend.hpp"
+BEGIN_CS_NAMESPACE
+using BLEMIDIBackend = ArduinoBLEBackend;
+END_CS_NAMESPACE
+#define CS_BLEMIDI_SUPPORTED 1
+#endif
+
+#ifdef CS_BLEMIDI_SUPPORTED
+#include "GenericBLEMIDI_Interface.hpp"
+BEGIN_CS_NAMESPACE
+using BluetoothMIDI_Interface = GenericBLEMIDI_Interface<BLEMIDIBackend>;
+END_CS_NAMESPACE
+#endif
