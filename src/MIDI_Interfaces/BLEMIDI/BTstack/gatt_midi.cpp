bc061346f1de804b366fc2fb77f0c3d0b182f55b
Pieter Pas <pieter.pas@outlook.com>
2024-01-23 00:45:17 +0100

    Further improve Pico W BLE backend

--- src/MIDI_Interfaces/BLEMIDI/BTstack/gatt_midi.cpp
+++ src/MIDI_Interfaces/BLEMIDI/BTstack/gatt_midi.cpp
@@ -6,7 +6,6 @@
 #include <cassert>
 #include <csignal>
 #include <cstdint>
-#include <cstdio>
 #include <cstdlib>
 #include <cstring>
 
@@ -20,6 +19,7 @@
 #include "hci_event_names.hpp"
 
 #include <AH/Arduino-Wrapper.h>
+#include <AH/Debug/Debug.hpp>
 
 namespace cs::midi_ble_btstack {
 
@@ -31,14 +31,6 @@ constexpr uint16_t midi_cccd_handle =
     ATT_CHARACTERISTIC_7772E5DB_3868_4112_A1A9_F2669D106BF3_01_CLIENT_CONFIGURATION_HANDLE;
 
 MIDIBLEInstance *instance = nullptr;
-
-struct {
-    BLEDataView data {};
-    uint16_t conn_handle = 0xFFFF;
-    uint16_t char_handle = 0xFFFF;
-} to_notify {};
-volatile sig_atomic_t notify_done = false;
-
 btstack_packet_callback_registration_t hci_event_callback_registration;
 
 void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet,
@@ -50,7 +42,7 @@ int att_write_callback(hci_con_handle_t con_handle, uint16_t att_handle,
                        uint16_t transaction_mode, uint16_t offset,
                        uint8_t *buffer, uint16_t buffer_size);
 
-const uint8_t adv_data[] {
+uint8_t adv_data[] {
     // Flags general discoverable
     0x02, BLUETOOTH_DATA_TYPE_FLAGS, 0x06,
     // Name
@@ -65,11 +57,10 @@ void le_midi_setup_adv() {
     uint16_t adv_int_min = 0x0030;
     uint16_t adv_int_max = 0x0030;
     uint8_t adv_type = 0;
-    bd_addr_t null_addr;
-    memset(null_addr, 0, 6);
+    bd_addr_t null_addr {};
     gap_advertisements_set_params(adv_int_min, adv_int_max, adv_type, 0,
                                   null_addr, 0x07, 0x00);
-    gap_advertisements_set_data(sizeof(adv_data), (uint8_t *)adv_data);
+    gap_advertisements_set_data(sizeof(adv_data), adv_data);
     gap_advertisements_enable(1);
 }
 
@@ -89,7 +80,7 @@ void le_midi_setup() {
 }
 
 // HCI_SUBEVENT_LE_CONNECTION_COMPLETE
-void connection_handler(uint8_t *packet, uint16_t size) {
+void connection_handler(uint8_t *packet, [[maybe_unused]] uint16_t size) {
     if (!instance)
         return;
     if (hci_subevent_le_connection_complete_get_status(packet) != 0)
@@ -98,96 +89,99 @@ void connection_handler(uint8_t *packet, uint16_t size) {
         hci_subevent_le_connection_complete_get_connection_handle(packet);
     instance->handleConnect(BLEConnectionHandle {conn_handle});
 }
+// HCI_SUBEVENT_LE_CONNECTION_UPDATE_COMPLETE
+void connection_update_handler(uint8_t *packet,
+                               [[maybe_unused]] uint16_t size) {
+    DEBUGREF( // clang-format off
+        "Connection update: status="
+        << hci_subevent_le_connection_update_complete_get_status(packet)
+        << ", connection interval="
+        << hci_subevent_le_connection_update_complete_get_conn_interval(packet)
+        << ", connection latency="
+        << hci_subevent_le_connection_update_complete_get_conn_latency(packet)
+        << ", supervision timeout="
+        << hci_subevent_le_connection_update_complete_get_supervision_timeout(packet));
+              // clang-format on
+}
+// HCI_SUBEVENT_LE_REMOTE_CONNECTION_PARAMETER_REQUEST
+void connection_param_req_handler(uint8_t *packet,
+                                  [[maybe_unused]] uint16_t size) {
+    DEBUGREF( // clang-format off
+        "Connection parameter request: interval min="
+        << hci_subevent_le_remote_connection_parameter_request_get_interval_min(packet)
+        << ", interval max="
+        << hci_subevent_le_remote_connection_parameter_request_get_interval_max(packet)
+        << ", latency="
+        << hci_subevent_le_remote_connection_parameter_request_get_latency(packet)
+        << ", timeout="
+        << hci_subevent_le_remote_connection_parameter_request_get_timeout(packet));
+              // clang-format on
+}
 // HCI_EVENT_LE_META
 void le_packet_handler(uint8_t *packet, uint16_t size) {
     uint8_t type = hci_event_le_meta_get_subevent_code(packet);
-    printf("LE event: %s (0x%02x)\n", le_event_names[type], (int)type);
+    DEBUGREF("LE event: " << le_event_names[type] << " (0x" << hex << type
+                          << dec << ")");
     switch (type) {
         case HCI_SUBEVENT_LE_CONNECTION_COMPLETE:
             connection_handler(packet, size);
             break;
         case HCI_SUBEVENT_LE_CONNECTION_UPDATE_COMPLETE:
-            printf(
-                "Connection update: status=%d, connection interval=%d, "
-                "connection latency=%d, supervision timeout=%d\n",
-                hci_subevent_le_connection_update_complete_get_status(packet),
-                hci_subevent_le_connection_update_complete_get_conn_interval(
-                    packet),
-                hci_subevent_le_connection_update_complete_get_conn_latency(
-                    packet),
-                hci_subevent_le_connection_update_complete_get_supervision_timeout(
-                    packet));
+            connection_update_handler(packet, size);
             break;
         case HCI_SUBEVENT_LE_REMOTE_CONNECTION_PARAMETER_REQUEST:
-            printf(
-                "Connection parameter request: interval min=%d, interval "
-                "max=%d, "
-                "latency=%d, timeout=%d\n",
-                hci_subevent_le_remote_connection_parameter_request_get_interval_min(
-                    packet),
-                hci_subevent_le_remote_connection_parameter_request_get_interval_max(
-                    packet),
-                hci_subevent_le_remote_connection_parameter_request_get_latency(
-                    packet),
-                hci_subevent_le_remote_connection_parameter_request_get_timeout(
-                    packet));
+            connection_param_req_handler(packet, size);
             break;
         default: break;
     }
 }
 // HCI_EVENT_LE_META
-void gattservice_handler(uint8_t *packet, uint16_t size) {
+void gattservice_handler(uint8_t *packet, [[maybe_unused]] uint16_t size) {
     uint8_t type = hci_event_gattservice_meta_get_subevent_code(packet);
-    printf("GATT service event: %s (0x%02x)\n", gattservice_event_names[type],
-           (int)type);
+    DEBUGREF("GATT service event: " << gattservice_event_names[type] << " (0x"
+                                    << hex << type << dec << ")");
 }
 // HCI_EVENT_DISCONNECTION_COMPLETE
-void disconnect_handler(uint8_t *packet, uint16_t size) {
+void disconnect_handler(uint8_t *packet, [[maybe_unused]] uint16_t size) {
     assert(instance);
     uint16_t conn_handle =
         hci_event_disconnection_complete_get_connection_handle(packet);
     instance->handleDisconnect(BLEConnectionHandle {conn_handle});
 }
-// ATT_EVENT_CAN_SEND_NOW
-void can_send_now_handler() {
-    if (!to_notify.data)
-        return;
-    att_server_notify(to_notify.conn_handle, to_notify.char_handle,
-                      to_notify.data.data, to_notify.data.length);
-    std::atomic_signal_fence(std::memory_order_release);
-    notify_done = true;
-}
 // ATT_EVENT_MTU_EXCHANGE_COMPLETE
-void mtu_exchange_complete_handler(uint8_t *packet, uint16_t size) {
+void mtu_exchange_complete_handler(uint8_t *packet,
+                                   [[maybe_unused]] uint16_t size) {
     assert(instance);
     uint16_t conn_handle = att_event_mtu_exchange_complete_get_handle(packet);
     uint16_t mtu = att_event_mtu_exchange_complete_get_MTU(packet);
-    printf("MTU: %d\n", (int)mtu);
+    DEBUGREF("mtu=" << mtu);
     instance->handleMTU(BLEConnectionHandle {conn_handle}, mtu);
 }
 // GATT_EVENT_MTU
-void gatt_event_mtu_handler(uint8_t *packet, uint16_t size) {
+void gatt_event_mtu_handler(uint8_t *packet, [[maybe_unused]] uint16_t size) {
     assert(instance);
     uint16_t conn_handle = gatt_event_mtu_get_handle(packet);
     uint16_t mtu = gatt_event_mtu_get_MTU(packet);
     instance->handleMTU(BLEConnectionHandle {conn_handle}, mtu);
 }
 // BTSTACK_EVENT_STATE
-void btstack_event_state_handler(uint8_t *packet, uint16_t size) {
+void btstack_event_state_handler(uint8_t *packet,
+                                 [[maybe_unused]] uint16_t size) {
     if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING)
         return;
     bd_addr_t local_addr;
     gap_local_bd_addr(local_addr);
-    printf("BTstack up and running on %s.\n", bd_addr_to_str(local_addr));
+    DEBUGREF("BTstack up and running on " << bd_addr_to_str(local_addr));
 }
 
 // Handles all HCI event packets.
-void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet,
-                    uint16_t size) {
+void packet_handler(uint8_t packet_type, [[maybe_unused]] uint16_t channel,
+                    uint8_t *packet, uint16_t size) {
     if (packet_type != HCI_EVENT_PACKET)
         return;
     auto type = hci_event_packet_get_type(packet);
-    printf("HCI event: %s (0x%02x)\n", hci_event_names[type], (int)type);
+    DEBUGREF("HCI event: " << hci_event_names[type] << " (0x" << hex << type
+                           << dec << ")");
     switch (type) {
         case HCI_EVENT_LE_META: le_packet_handler(packet, size); break;
         case HCI_EVENT_GATTSERVICE_META:
@@ -196,7 +190,6 @@ void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet,
         case HCI_EVENT_DISCONNECTION_COMPLETE:
             disconnect_handler(packet, size);
             break;
-        case ATT_EVENT_CAN_SEND_NOW: can_send_now_handler(); break;
         default: break;
         case ATT_EVENT_MTU_EXCHANGE_COMPLETE:
             mtu_exchange_complete_handler(packet, size);
@@ -248,7 +241,8 @@ int midi_value_write(hci_con_handle_t conn_handle, uint8_t *buffer,
 int att_write_callback(hci_con_handle_t conn_handle, uint16_t att_handle,
                        [[maybe_unused]] uint16_t transaction_mode,
                        uint16_t offset, uint8_t *buffer, uint16_t buffer_size) {
-    Serial << "att_write_callback: " << att_handle << endl;
+    DEBUGREF("ATT write: handle=" << att_handle << ", offset=" << offset
+                                  << ", size=" << buffer_size);
     // Only support regular writes (no prepared/long writes)
     if (transaction_mode != ATT_TRANSACTION_MODE_NONE)
         return ATT_ERROR_REQUEST_NOT_SUPPORTED;
@@ -265,13 +259,11 @@ int att_write_callback(hci_con_handle_t conn_handle, uint16_t att_handle,
 }
 
 template <class F>
-uint32_t cyw43_arch_async_context_execute_sync_callable(F &&f) {
-    return async_context_execute_sync(
-        cyw43_arch_async_context(),
-        +[](void *arg) -> uint32_t {
-            return (*static_cast<std::remove_reference_t<F> *>(arg))();
-        },
-        &f);
+btstack_context_callback_registration_t create_context_callback(F &f) {
+    btstack_context_callback_registration_t ret {};
+    ret.callback = +[](void *context) { (*static_cast<F *>(context))(); };
+    ret.context = &f;
+    return ret;
 }
 
 } // namespace
@@ -286,28 +278,41 @@ bool init(MIDIBLEInstance &instance, BLESettings settings) {
 
 void notify(BLEConnectionHandle conn_handle,
             BLECharacteristicHandle char_handle, BLEDataView data) {
+    // DEBUGREF("[" << data.length << "] "
+    //              << (AH::HexDump {data.data, data.length}));
+    [[maybe_unused]] const auto t0 = micros();
     // Don't bother sending empty packets
     if (!data)
         return;
+    // Flag to know when the can-send-now callback is done
+    volatile std::atomic_bool notify_done {false};
+    // The following is executed in the BTstack can-send-now callback, so it is
+    // synchronized with the BTstack code
+    auto send = [&] {
+        DEBUGREF("notify " << micros() - t0);
+        att_server_notify(conn_handle.conn, char_handle.characteristic,
+                          data.data, data.length);
+        DEBUGREF("notify done " << micros() - t0);
+        notify_done.store(true, std::memory_order_release);
+    };
+    auto send_ctx = create_context_callback(send);
     // The following is executed in the async_context, so it is synchronized
     // with the BTstack code
     auto run = [&] {
-        // Store the data to send
-        to_notify.data = data;
-        to_notify.conn_handle = conn_handle.conn;
-        to_notify.char_handle = char_handle.characteristic;
-        // Request can-send-now event to be fired later
-        att_server_request_can_send_now_event(to_notify.conn_handle);
-        return 0;
+        // Request can-send-now callback to be fired later
+        DEBUGREF("req send " << micros() - t0);
+        auto ret = att_server_request_to_send_notification(&send_ctx,
+                                                           conn_handle.conn);
+        assert(ret == ERROR_CODE_SUCCESS);
     };
-    // Flag to know when the can-send-now event is done
-    notify_done = false;
-    std::atomic_signal_fence(std::memory_order_release);
-    // Run the code above code in the async_context (synchronously)
-    cyw43_arch_async_context_execute_sync_callable(run);
-    // Wait for the can-send-now event to clear the flag
-    while (!notify_done) tight_loop_contents();
-    std::atomic_signal_fence(std::memory_order_acquire);
+    auto run_ctx = create_context_callback(run);
+    // Schedule the run callback on the BTstack thread, which will then schedule
+    // the send callback as soon as sending data is possible.
+    DEBUGREF("req main thread " << micros() - t0);
+    btstack_run_loop_execute_on_main_thread(&run_ctx);
+    // Wait for the can-send-now callback to clear the flag
+    while (!notify_done.load(std::memory_order_acquire)) tight_loop_contents();
+    DEBUGREF("all done " << micros() - t0);
 }
 
 } // namespace cs::midi_ble_btstack
