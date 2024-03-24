567d964e0e9d886ff91c8efb81fb1c51aabf29b7
Pieter Pas <pieter.pas@outlook.com>
2024-03-24 17:03:15 +0100

    Add increaseBitDepthMiddle

--- test/AH/Math/test-IncreaseBitDepth.cpp
+++ test/AH/Math/test-IncreaseBitDepth.cpp
@@ -21,3 +21,35 @@ TEST(IncreaseBitDepth, IncreaseBitDepth) {
     };
     EXPECT_EQ(out, expected);
 }
+
+TEST(IncreaseBitDepth, IncreaseBitDepthMiddle) {
+    std::array<uint8_t, 8> in = {
+        0b000, 0b001, 0b010, 0b011, 0b100, 0b101, 0b110, 0b111,
+    };
+    std::array<uint8_t, 8> out;
+    std::transform(
+        std::begin(in), std::end(in), std::begin(out), [](uint8_t in) {
+            return increaseBitDepthMiddle<7, 3, uint8_t, uint8_t>(in);
+        });
+    std::array<uint8_t, 8> expected = {
+        0b000'0000, 0b001'0000, 0b010'0000, 0b011'0000,
+        0b100'0000, 0b101'0101, 0b110'1010, 0b111'1111,
+    };
+    EXPECT_EQ(out, expected);
+    EXPECT_EQ((increaseBitDepthMiddle<2, 2, uint32_t, uint32_t>(3)), 3);
+    EXPECT_EQ((increaseBitDepthMiddle<3, 2, uint32_t, uint32_t>(3)), 7);
+    EXPECT_EQ((increaseBitDepthMiddle<4, 2, uint32_t, uint32_t>(3)), 15);
+    EXPECT_EQ((increaseBitDepthMiddle<5, 2, uint32_t, uint32_t>(3)), 31);
+    EXPECT_EQ((increaseBitDepthMiddle<32, 2, uint32_t, uint32_t>(3)),
+              0xFFFFFFFF);
+    EXPECT_EQ((increaseBitDepthMiddle<2, 2, uint32_t, uint32_t>(2)), 2);
+    EXPECT_EQ((increaseBitDepthMiddle<3, 2, uint32_t, uint32_t>(2)), 4);
+    EXPECT_EQ((increaseBitDepthMiddle<4, 2, uint32_t, uint32_t>(2)), 8);
+    EXPECT_EQ((increaseBitDepthMiddle<5, 2, uint32_t, uint32_t>(2)), 16);
+    EXPECT_EQ((increaseBitDepthMiddle<32, 2, uint32_t, uint32_t>(2)),
+              0x80000000);
+    EXPECT_EQ((increaseBitDepthMiddle<32, 10, uint32_t, uint32_t>(512)),
+              0x80000000);
+    EXPECT_EQ((increaseBitDepthMiddle<32, 10, uint32_t, uint32_t>(1023)),
+              0xFFFFFFFF);
+}
