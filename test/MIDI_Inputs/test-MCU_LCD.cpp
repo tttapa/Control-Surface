a28a0585d7b2c9805acb3303e72bda09c31b1d7c
Pieter Pas <pieter.p.dev@outlook.com>
2024-10-27 12:12:47 +0100

    [Test] check LCD dirty flag

--- test/MIDI_Inputs/test-MCU_LCD.cpp
+++ test/MIDI_Inputs/test-MCU_LCD.cpp
@@ -10,32 +10,40 @@ TEST(LCD, Ranges) {
         std::vector<uint8_t> sysex = {
             0xF0, 0x00, 0x00, 0x66, 0x10, 0x12, 0x00, 'a', 'b', 'c', 'd', 0xF7,
         };
+        lcd.clearDirty();
         MIDIInputElementSysEx::updateAllWith(sysex);
         EXPECT_STREQ(lcd.getText(), "abcd");
+        EXPECT_TRUE(lcd.getDirty());
     }
     { // Buffer longer, MIDI in middle of buffer
         MCU::LCD<4> lcd(0);
         std::vector<uint8_t> sysex = {
             0xF0, 0x00, 0x00, 0x66, 0x10, 0x12, 0x01, 'b', 'c', 0xF7,
         };
+        lcd.clearDirty();
         MIDIInputElementSysEx::updateAllWith(sysex);
         EXPECT_STREQ(lcd.getText(), " bc ");
+        EXPECT_TRUE(lcd.getDirty());
     }
     { // MIDI overlaps with start of buffer
         MCU::LCD<4> lcd(1);
         std::vector<uint8_t> sysex = {
             0xF0, 0x00, 0x00, 0x66, 0x10, 0x12, 0x00, 'a', 'b', 0xF7,
         };
+        lcd.clearDirty();
         MIDIInputElementSysEx::updateAllWith(sysex);
         EXPECT_STREQ(lcd.getText(), "b   ");
+        EXPECT_TRUE(lcd.getDirty());
     }
     { // MIDI overlaps with end of buffer
         MCU::LCD<4> lcd(0);
         std::vector<uint8_t> sysex = {
             0xF0, 0x00, 0x00, 0x66, 0x10, 0x12, 0x03, 'd', 'e', 0xF7,
         };
+        lcd.clearDirty();
         MIDIInputElementSysEx::updateAllWith(sysex);
         EXPECT_STREQ(lcd.getText(), "   d");
+        EXPECT_TRUE(lcd.getDirty());
     }
     { // MIDI longer
         MCU::LCD<4> lcd(1);
@@ -43,24 +51,31 @@ TEST(LCD, Ranges) {
             0xF0, 0x00, 0x00, 0x66, 0x10, 0x12, 0x00,
             'a',  'b',  'c',  'd',  'e',  'f',  0xF7,
         };
+        lcd.clearDirty();
         MIDIInputElementSysEx::updateAllWith(sysex);
         EXPECT_STREQ(lcd.getText(), "bcde");
+        EXPECT_TRUE(lcd.getDirty());
     }
     { // No overlap, buffer lower
         MCU::LCD<4> lcd(0);
         std::vector<uint8_t> sysex = {
             0xF0, 0x00, 0x00, 0x66, 0x10, 0x12, 0x04, 'e', 'f', 'g', 'h', 0xF7,
         };
+        lcd.clearDirty();
         MIDIInputElementSysEx::updateAllWith(sysex);
         EXPECT_STREQ(lcd.getText(), "    ");
+        EXPECT_FALSE(lcd.getDirty());
+        EXPECT_FALSE(lcd.getDirty());
     }
     { // No overlap, MIDI lower
         MCU::LCD<4> lcd(4);
         std::vector<uint8_t> sysex = {
             0xF0, 0x00, 0x00, 0x66, 0x10, 0x12, 0x00, 'a', 'b', 'c', 'd', 0xF7,
         };
+        lcd.clearDirty();
         MIDIInputElementSysEx::updateAllWith(sysex);
         EXPECT_STREQ(lcd.getText(), "    ");
+        EXPECT_FALSE(lcd.getDirty());
     }
 }
 
@@ -72,8 +87,11 @@ TEST(LCD, FillOneBufferWithManyMIDIMessages) {
         {0xF0, 0x00, 0x00, 0x66, 0x10, 0x12, 0x08, 'i', 'j', 'k', 'l', 0xF7},
         {0xF0, 0x00, 0x00, 0x66, 0x10, 0x12, 0x0C, 'm', 'n', 'o', 'p', 0xF7},
     };
-    for (auto sysex : sysexs)
+    for (auto sysex : sysexs) {
+        lcd.clearDirty();
         MIDIInputElementSysEx::updateAllWith(sysex);
+        EXPECT_TRUE(lcd.getDirty());
+    }
     EXPECT_STREQ(lcd.getText(), "abcd"
                                 "efgh"
                                 "ijkl"
@@ -82,6 +100,7 @@ TEST(LCD, FillOneBufferWithManyMIDIMessages) {
 
 TEST(LCD, FillManyBuffersWithOneMIDIMessage) {
     MCU::LCD<4> lcds[] = {0x0, 0x4, 0x8, 0xC};
+    for (auto &lcd : lcds) lcd.clearDirty();
     std::vector<uint8_t> sysex = {
         0xF0, 0x00, 0x00, 0x66, 0x10, 0x12, 0x00, 'a', 'b', 'c', 'd', 'e',
         'f',  'g',  'h',  'i',  'j',  'k',  'l',  'm', 'n', 'o', 'p', 0xF7,
@@ -91,6 +110,7 @@ TEST(LCD, FillManyBuffersWithOneMIDIMessage) {
     EXPECT_STREQ(lcds[1].getText(), "efgh");
     EXPECT_STREQ(lcds[2].getText(), "ijkl");
     EXPECT_STREQ(lcds[3].getText(), "mnop");
+    for (auto &lcd : lcds) EXPECT_TRUE(lcd.getDirty());
 }
 
 TEST(LCDlength, len) {
