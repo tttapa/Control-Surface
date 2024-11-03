c9c364be84501435cc74afb27187e56409b07edf
Pieter Pas <pieter.p.dev@outlook.com>
2024-11-03 12:43:28 +0100

    Enable Nano Every test sketches

--- examples/test/STL-test/STL-test.ino
+++ examples/test/STL-test/STL-test.ino
@@ -1,4 +1,4 @@
 /**
- * @boards  AVR, AVR USB, Nano 33 IoT, Nano 33 BLE, UNO R4, Pi Pico, Teensy 3.x, ESP8266, ESP32
+ * @boards  AVR, AVR USB, Nano Every, Nano 33 IoT, Nano 33 BLE, UNO R4, Pi Pico, Teensy 3.x, ESP8266, ESP32
  */
 #include <Arduino_Helpers.h>
